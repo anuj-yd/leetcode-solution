d86fb0b69c6e8574c8ec0b00650a6b88e0de69fe
Anuj Yadav <150239759+anuj-yd@users.noreply.github.com>
2025-02-16 21:05:18 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (15.83%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.java
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.java
@@ -3,7 +3,7 @@ class Solution {
         if(n==0)return n;
         int count = 0;
         while (n > 0) {
-            n /= 5;
+            n /= 5; // Count the number of multiples of 5, 25, 125, etc.
             count += n;
         }
         return count;
