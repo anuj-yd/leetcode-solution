05b653eace047d5543d2cdee4521fd929d027d90
Anuj Yadav <150239759+anuj-yd@users.noreply.github.com>
2025-02-20 02:13:17 +0530

    Update check-if-two-string-arrays-are-equivalent.java

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
@@ -2,22 +2,16 @@ class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < word1.length; i++) {
-            sb.append(word1[i]); // Append the string at index i, not charAt
+            sb.append(word1[i]); 
         }
         
         StringBuilder sb1 = new StringBuilder();
         for (int i = 0; i < word2.length; i++) {
-            sb1.append(word2[i]); // Append the string at index i, not charAt
+            sb1.append(word2[i]); 
         }
         
-        String str1 = sb.toString(); // Added semicolon
-        String str2 = sb1.toString(); // Fixed syntax and added semicolon
+        String str1 = sb.toString(); 
+        String str2 = sb1.toString(); 
         
-        return str1.equals(str2); // Correct string comparison
-    }
+        return str1.equals(str2);     }
 }
-
-        // String str1 = String.join("", word1);
-        // String str2 = String.join("", word2);
-        
-        // return str1.equals(str2);}
