03c6bdcc02a275db1ed851bef2e3dcc09b51f520
Anuj Yadav <150239759+anuj-yd@users.noreply.github.com>
2025-04-01 11:54:10 +0530

    Time: 1 ms (27.22%) | Memory: 50.3 MB (52.73%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -3,7 +3,7 @@ class Solution {
         int i=0;
         int j=0;
         int n = nums.length;
-        while(n!=0){
+        while(n>0){
             j=j^nums[i];
             i++;
             n--;
