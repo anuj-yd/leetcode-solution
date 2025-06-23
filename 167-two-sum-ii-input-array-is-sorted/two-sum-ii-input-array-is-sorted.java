9a28e3909ecb42ab99f7e91ee4943974113bef26
Anuj Yadav <150239759+anuj-yd@users.noreply.github.com>
2025-06-23 15:42:56 +0530

    Time: 2 ms (93.17%) | Memory: 47.2 MB (50.77%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -13,5 +13,20 @@ class Solution {
             }
         }
         return new int[0];
+
+        // for(int i=0;i<numbers.length;i++){
+        //     for(int j=numbers.length-1;j>0;j--){
+        //         int sum = numbers[i]+numbers[j];
+        //         if(sum == target){
+        //             return new int[]{i+1,j+1};
+        //         }else if(sum>target){
+        //             j--;
+        //         }else{
+        //             i++;
+        //         }
+        //     }
+        // }
+        // return new int[0];
+
     }
 }
