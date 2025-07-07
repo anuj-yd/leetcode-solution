8c22423f71f51925d1bef8cdb282b4e7b8319e25
Anuj Yadav <150239759+anuj-yd@users.noreply.github.com>
2025-07-07 13:23:34 +0530

    Time: 22 ms (78.88%) | Memory: 57.5 MB (12.76%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,63 +1,34 @@
-import java.util.Random;
-
 class Solution {
-    private Random rand = new Random();
-
     public int[] sortArray(int[] nums) {
-        quickSort(nums, 0, nums.length - 1);
+        if (nums == null || nums.length <= 1) return nums;
+        mergeSort(nums, 0, nums.length - 1);
         return nums;
     }
 
-    private void quickSort(int[] arr, int low, int high) {
-        if (low >= high) return;
-
-        if (high - low <= 16) { // increased threshold
-            insertionSort(arr, low, high);
-            return;
-        }
-
-        int pivotIndex = randomizedPartition(arr, low, high);
-        quickSort(arr, low, pivotIndex - 1);
-        quickSort(arr, pivotIndex + 1, high);
+    private void mergeSort(int[] arr, int left, int right) {
+        if (left >= right) return;
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
     }
 
-    private int randomizedPartition(int[] arr, int low, int high) {
-        int pivotIndex = low + rand.nextInt(high - low + 1);
-        swap(arr, pivotIndex, high);
-        return partition(arr, low, high);
-    }
+    private void merge(int[] arr, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+        int i = left, j = mid + 1, k = 0;
 
-    private int partition(int[] arr, int low, int high) {
-        int pivot = arr[high];
-        int i = low - 1;
-
-        for (int j = low; j < high; j++) {
-            if (arr[j] <= pivot) {
-                i++;
-                swap(arr, i, j);
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
             }
         }
-        swap(arr, i + 1, high);
-        return i + 1;
-    }
 
-    private void insertionSort(int[] arr, int low, int high) {
-        for (int i = low + 1; i <= high; i++) {
-            int key = arr[i];
-            int j = i - 1;
-            while (j >= low && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j--;
-            }
-            arr[j + 1] = key;
-        }
-    }
+        while (i <= mid) temp[k++] = arr[i++];
+        while (j <= right) temp[k++] = arr[j++];
 
-    private void swap(int[] arr, int i, int j) {
-        if (i != j) {
-            int tmp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = tmp;
-        }
+        // Copy back sorted temp array to original
+        System.arraycopy(temp, 0, arr, left, temp.length);
     }
 }
