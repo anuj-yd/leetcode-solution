1571066083420a972046d3b55d0fcdeb851582e1
Anuj Yadav <150239759+anuj-yd@users.noreply.github.com>
2025-07-07 13:20:22 +0530

    Time: 2218 ms (5.00%) | Memory: 64.8 MB (5.09%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,47 +1,63 @@
-import java.util.ArrayList;
+import java.util.Random;
 
 class Solution {
+    private Random rand = new Random();
 
-    public void merge(int[] arr, int s, int mid, int e) {
-        ArrayList<Integer> ans = new ArrayList<>();
-        int i = s;
-        int j = mid + 1;
+    public int[] sortArray(int[] nums) {
+        quickSort(nums, 0, nums.length - 1);
+        return nums;
+    }
 
-        while (i <= mid && j <= e) {
-            if (arr[i] < arr[j]) {
-                ans.add(arr[i]);
+    private void quickSort(int[] arr, int low, int high) {
+        if (low >= high) return;
+
+        if (high - low <= 16) { // increased threshold
+            insertionSort(arr, low, high);
+            return;
+        }
+
+        int pivotIndex = randomizedPartition(arr, low, high);
+        quickSort(arr, low, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, high);
+    }
+
+    private int randomizedPartition(int[] arr, int low, int high) {
+        int pivotIndex = low + rand.nextInt(high - low + 1);
+        swap(arr, pivotIndex, high);
+        return partition(arr, low, high);
+    }
+
+    private int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
                 i++;
-            } else {
-                ans.add(arr[j]);
-                j++;
+                swap(arr, i, j);
             }
         }
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
 
-        while (i <= mid) {
-            ans.add(arr[i]);
-            i++;
-        }
-        while (j <= e) {
-            ans.add(arr[j]);
-            j++;
-        }
-
-        for (int idx = 0; idx < ans.size(); idx++) {
-            arr[s + idx] = ans.get(idx);
+    private void insertionSort(int[] arr, int low, int high) {
+        for (int i = low + 1; i <= high; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= low && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
         }
     }
 
-    public void mergeSort(int[] arr, int s, int e) {
-        if (s < e) {
-            int mid = s + (e - s) / 2;
-            mergeSort(arr, s, mid);
-            mergeSort(arr, mid + 1, e);
-            merge(arr, s, mid, e);
+    private void swap(int[] arr, int i, int j) {
+        if (i != j) {
+            int tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
         }
-    }
-
-    public int[] sortArray(int[] arr) {
-        mergeSort(arr, 0, arr.length - 1);
-        return arr;
     }
 }
