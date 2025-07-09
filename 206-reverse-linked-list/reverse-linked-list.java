5d3197aea879d1f857916ae1c6b60e839d8947c4
Anuj Yadav <150239759+anuj-yd@users.noreply.github.com>
2025-07-09 10:43:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (82.25%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -10,10 +10,28 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head){
-        if(head==null||head.next == null) return head;
-        ListNode temp = reverseList(head.next);
-        head.next.next = head;
-        head.next = null;
-        return temp;  
+        // Using Recursion (this not optimal tc is O(n) and sc is also O(n))
+
+        // if(head==null||head.next == null) return head;
+        // ListNode temp = reverseList(head.next);
+        // head.next.next = head;
+        // head.next = null;
+        // return temp;  
+
+        // Optimal is Itrative or 3 pointer approch O(1)  
+        ListNode curr = head;
+        ListNode temp = null;
+        ListNode prev = null;   
+        while(curr!=null){
+            temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+
+
+
+
     }
 }
