07506ba9ca190f802494331672ec233814349914
Anuj Yadav <150239759+anuj-yd@users.noreply.github.com>
2025-07-10 18:54:55 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (9.55%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.java
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -10,21 +10,45 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        if (head == null || head.next == null) return head;
+        // 1 base indexing h or first node odd consider kiya gya h
+        // ListNode odd = head;
+        // ListNode even = head.next;
+        // ListNode evenHead = even;
 
-        ListNode odd = head;
-        ListNode even = head.next;
-        ListNode evenHead = even;
+        // while (even != null && even.next != null) {
+        //     odd.next = even.next;
+        //     odd = odd.next;
 
-        while (even != null && even.next != null) {
-            odd.next = even.next;
-            odd = odd.next;
+        //     even.next = odd.next;
+        //     even = even.next;
+        // }
 
-            even.next = odd.next;
-            even = even.next;
+        // odd.next = evenHead;
+        // return head;
+        ListNode odd = new ListNode(0);
+        ListNode even = new ListNode(0);
+
+        ListNode tempo = odd;
+        ListNode tempe = even;
+
+        ListNode temp = head;
+
+        while(temp!=null){
+            tempo.next = temp;
+            temp = temp.next;
+            tempo = tempo.next;
+
+            tempe.next = temp;
+            if(temp == null) break;
+            temp = temp.next;
+            tempe = tempe.next;
         }
+        
+        odd = odd.next;
+        even = even.next;
+        tempo.next=even;
+        return odd;
 
-        odd.next = evenHead;
-        return head;
+
     }
 }
