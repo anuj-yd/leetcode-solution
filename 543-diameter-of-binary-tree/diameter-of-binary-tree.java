5e28a8f326dcf7ef4483c05a4c97ddc1269b1d39
Anuj Yadav <150239759+anuj-yd@users.noreply.github.com>
2025-07-17 11:37:34 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (29.13%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -13,22 +13,43 @@
  *     }
  * }
  */
+// class Solution {
+//     public int height(TreeNode root){
+//         if(root == null || root.left == null && root.right==null) return 0;
+//         return 1+Math.max(height(root.left),height(root.right));
+//     }
+//     public int diameterOfBinaryTree(TreeNode root){
+//         if(root == null || root.left == null && root.right==null) return 0;
+
+//         int leftroot = diameterOfBinaryTree(root.left);
+//         int rightroot = diameterOfBinaryTree(root.right);
+
+//         int mid = height(root.left)+height(root.right);
+//         if(root.left!=null) mid++;
+//         if(root.right!=null) mid++;
+
+//         return Math.max(leftroot,Math.max(rightroot,mid));
+
+//     }
+// }
+
 class Solution {
-    public int height(TreeNode root){
-        if(root == null || root.left == null && root.right==null) return 0;
-        return 1+Math.max(height(root.left),height(root.right));
+    int d=0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        
+        maxheight(root);
+        return d;
+
     }
-    public int diameterOfBinaryTree(TreeNode root){
-        if(root == null || root.left == null && root.right==null) return 0;
-
-        int leftroot = diameterOfBinaryTree(root.left);
-        int rightroot = diameterOfBinaryTree(root.right);
-
-        int mid = height(root.left)+height(root.right);
-        if(root.left!=null) mid++;
-        if(root.right!=null) mid++;
-
-        return Math.max(leftroot,Math.max(rightroot,mid));
-
+    private int maxheight(TreeNode root)
+    {
+        if(root==null)
+        {
+            return 0;
+        }
+        int lh=maxheight(root.left);
+        int rh=maxheight(root.right);
+        d=Math.max(d,lh+rh);
+        return 1+Math.max(lh,rh);
     }
 }
