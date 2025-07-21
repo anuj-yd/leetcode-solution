aae8f068c73ac400bdf6040d043cbd67f13b23b4
Anuj Yadav <150239759+anuj-yd@users.noreply.github.com>
2025-07-21 21:27:48 +0530

    Time: 17 ms (18.13%) | Memory: 44.7 MB (34.05%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,14 +1,36 @@
-
 class Solution {
-    public boolean isAnagram(String s, String t){
-        if(s.length()!=t.length()) return false;
+    public boolean isAnagram(String s, String t) {
+        // boolean b = false;
+        // if(s.campareTo(t)==0){
+        //     b=true;
+        // }
+        // return b;
 
-        char[] sa = s.toCharArray();
-        char[] ta = t.toCharArray();
+        if (s.length() != t.length()) {
+            return false;
+        }
 
-        Arrays.sort(sa);
-        Arrays.sort(ta);
+        // Step 2: Frequency map for s
+        HashMap<Character, Integer> mp = new HashMap<>();
 
-        return Arrays.equals(sa, ta);
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            mp.put(ch, mp.getOrDefault(ch, 0) + 1);
+        }
+
+        // Step 3: Reduce frequency for each character in t
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            if (!mp.containsKey(ch)) {
+                return false; // character t me hai par s me nahi
+            }
+            mp.put(ch, mp.get(ch) - 1);
+            if (mp.get(ch) == 0) {
+                mp.remove(ch); // unnecessary key remove
+            }
+        }
+
+        // Step 4: Map should be empty if perfect match
+        return mp.isEmpty();
     }
 }
