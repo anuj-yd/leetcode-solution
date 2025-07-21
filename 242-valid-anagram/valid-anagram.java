e42ba584afca887b529daad40a5d4aa1f2f9d28e
Anuj Yadav <150239759+anuj-yd@users.noreply.github.com>
2025-07-21 21:30:40 +0530

    Time: 4 ms (77.38%) | Memory: 45 MB (16.86%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,36 +1,41 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // boolean b = false;
-        // if(s.campareTo(t)==0){
-        //     b=true;
-        // }
-        // return b;
 
-        if (s.length() != t.length()) {
-            return false;
-        }
+    //     if (s.length() != t.length()) {
+    //         return false;
+    //     }
 
-        // Step 2: Frequency map for s
-        HashMap<Character, Integer> mp = new HashMap<>();
+    //     // Step 2: Frequency map for s
+    //     HashMap<Character, Integer> mp = new HashMap<>();
 
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            mp.put(ch, mp.getOrDefault(ch, 0) + 1);
-        }
+    //     for (int i = 0; i < s.length(); i++) {
+    //         char ch = s.charAt(i);
+    //         mp.put(ch, mp.getOrDefault(ch, 0) + 1);
+    //     }
 
-        // Step 3: Reduce frequency for each character in t
-        for (int i = 0; i < t.length(); i++) {
-            char ch = t.charAt(i);
-            if (!mp.containsKey(ch)) {
-                return false; // character t me hai par s me nahi
-            }
-            mp.put(ch, mp.get(ch) - 1);
-            if (mp.get(ch) == 0) {
-                mp.remove(ch); // unnecessary key remove
-            }
-        }
+    //     // Step 3: Reduce frequency for each character in t
+    //     for (int i = 0; i < t.length(); i++) {
+    //         char ch = t.charAt(i);
+    //         if (!mp.containsKey(ch)) {
+    //             return false; // character t me hai par s me nahi
+    //         }
+    //         mp.put(ch, mp.get(ch) - 1);
+    //         if (mp.get(ch) == 0) {
+    //             mp.remove(ch); // unnecessary key remove
+    //         }
+    //     }
 
-        // Step 4: Map should be empty if perfect match
-        return mp.isEmpty();
+    //     return mp.isEmpty();
+    // }
+
+    if(s.length()!=t.length()) return false;
+
+        char[] sa = s.toCharArray();
+        char[] ta = t.toCharArray();
+
+        Arrays.sort(sa);
+        Arrays.sort(ta);
+
+        return Arrays.equals(sa, ta);
     }
 }
