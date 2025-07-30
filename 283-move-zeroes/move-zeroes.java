5acfbd4fae76880f7994b259bdfbac220c4b4f5c
Anuj Yadav <150239759+anuj-yd@users.noreply.github.com>
2025-07-30 11:19:44 +0530

    Time: 2 ms (80.04%) | Memory: 46.7 MB (5.75%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,25 +1,41 @@
+// class Solution {
+//     // Correct swap method that modifies the array
+//     public void swap(int[] nums, int l, int h){
+//         int temp = nums[l];
+//         nums[l] = nums[h];
+//         nums[h] = temp;
+//     }
+
+//     public void moveZeroes(int[] nums) {
+//         int i = 0;
+//         int j = i + 1;
+
+//         while (j < nums.length) {
+//             if (nums[i] == 0 && nums[j] != 0) {
+//                 swap(nums, i, j);
+//                 i++;
+//             }
+//             j++;
+//             if (nums[i] != 0) {
+//                 i++;
+//                 j = i + 1;
+//             }
+//         }
+//     }
+// }
+
 class Solution {
-    // Correct swap method that modifies the array
-    public void swap(int[] nums, int l, int h){
-        int temp = nums[l];
-        nums[l] = nums[h];
-        nums[h] = temp;
-    }
-
     public void moveZeroes(int[] nums) {
-        int i = 0;
-        int j = i + 1;
-
-        while (j < nums.length) {
-            if (nums[i] == 0 && nums[j] != 0) {
-                swap(nums, i, j);
+        int i=0;
+        for(int j=0;j<nums.length;j++){
+            if(nums[j] != 0){
+                if(i !=j){
+                    int temp=nums[i];
+                    nums[i] = nums[j];
+                    nums[j]=temp;
+                }
                 i++;
             }
-            j++;
-            if (nums[i] != 0) {
-                i++;
-                j = i + 1;
-            }
-        }
+        } 
     }
 }
