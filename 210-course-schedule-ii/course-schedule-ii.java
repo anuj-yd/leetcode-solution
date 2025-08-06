6f1e6e3425a892e3a85e9a34446a4ca28354de99
Anuj Yadav <150239759+anuj-yd@users.noreply.github.com>
2025-08-06 14:17:43 +0530

    Time: 4 ms (91.80%) | Memory: 46.1 MB (55.08%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,41 +1,93 @@
-class Solution {
-    public int[] findOrder(int numCourses, int[][] prerequisites){
+// class Solution {
+//     public int[] findOrder(int numCourses, int[][] prerequisites){
 
+//         int V = numCourses;
+//         List<List<Integer>> adj = new ArrayList<>();
+//         int[] indegree = new int[V];
+//         for(int i =0;i<V;i++) adj.add(new ArrayList<>());
+//         for(int pre[] : prerequisites){
+//             int u = pre[0];
+//             int v = pre[1];
+
+//             adj.get(v).add(u);
+//             indegree[u]++;
+//         }
+
+//         Queue<Integer> q = new LinkedList<>();
+        
+//         for(int i=0;i<V;i++){
+//             if(indegree[i]==0){
+//                 q.add(i);
+//             }
+//         }
+//         int [] ans = new int[V];
+//         int idx=0;
+//         while(!q.isEmpty()){
+//             int node = q.poll();
+//             ans[idx++]= node;
+//             for(int nbr : adj.get(node)){
+//                 indegree[nbr]--;
+//                 if(indegree[nbr]==0){
+//                     q.add(nbr);
+//                 }
+//             }
+//         }
+//         if (idx < V) return new int[0];
+//         return ans;
+        
+//     }
+
+    
+// }
+
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
         int V = numCourses;
         List<List<Integer>> adj = new ArrayList<>();
-        int[] indegree = new int[V];
-        for(int i =0;i<V;i++) adj.add(new ArrayList<>());
-        for(int pre[] : prerequisites){
+
+        for (int i = 0; i < V; i++) adj.add(new ArrayList<>());
+        for (int[] pre : prerequisites) {
             int u = pre[0];
             int v = pre[1];
-
-            adj.get(v).add(u);
-            indegree[u]++;
+            adj.get(v).add(u); // prerequisite v → course u
         }
 
-        Queue<Integer> q = new LinkedList<>();
-        
-        for(int i=0;i<V;i++){
-            if(indegree[i]==0){
-                q.add(i);
-            }
-        }
-        int [] ans = new int[V];
-        int idx=0;
-        while(!q.isEmpty()){
-            int node = q.poll();
-            ans[idx++]= node;
-            for(int nbr : adj.get(node)){
-                indegree[nbr]--;
-                if(indegree[nbr]==0){
-                    q.add(nbr);
+        boolean[] visited = new boolean[V];
+        boolean[] recVisited = new boolean[V];
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                if (helper(i, visited, recVisited, st, adj)) {
+                    return new int[0]; // \U0001f501 Cycle detected
                 }
             }
         }
-        if (idx < V) return new int[0];
+
+        int[] ans = new int[V];
+        for (int i = 0; i < V; i++) {
+            ans[i] = st.pop();
+        }
         return ans;
-        
     }
 
-    
+    public boolean helper(int curr, boolean[] visited, boolean[] recVisited,
+                          Stack<Integer> st, List<List<Integer>> adj) {
+        visited[curr] = true;
+        recVisited[curr] = true;
+
+        for (int nbr : adj.get(curr)) {
+            if (!visited[nbr]) {
+                if (helper(nbr, visited, recVisited, st, adj)) {
+                    return true; // cycle
+                }
+            } else if (recVisited[nbr]) {
+                return true; // cycle
+            }
+        }
+
+        recVisited[curr] = false;
+        st.push(curr);
+        return false;
+    }
 }
