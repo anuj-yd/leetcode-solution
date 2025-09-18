65751adc316a6c1ec9d89f3ee7858231efd0c485
Anuj Yadav <150239759+anuj-yd@users.noreply.github.com>
2025-09-18 11:37:41 +0530

    Time: 1 ms (72.34%) | Memory: 41.4 MB (44.36%) - LeetSync

--- 1848-sum-of-unique-elements/sum-of-unique-elements.java
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.java
@@ -1,21 +1,25 @@
-class Solution {
-    public int sumOfUnique(int[] nums){
-        int sum =0;
-        for(int i=0;i<nums.length;i++){
-            boolean b=true; 
-            for(int j=0;j<nums.length;j++){
-                if(i!=j&&nums[i]==nums[j]){
-                    b=false;
-                    break;
-                }
-            }
-            if(b){
-                sum+=nums[i];
-            }
+import java.util.*;
 
-        }
-        return sum;
+class Solution {
+    public int sumOfUnique(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        Set<Integer> dup = new HashSet<>();
         
+        for (int n : nums) {
+            if (seen.contains(n)) {
+                dup.add(n); // agar pehle se hai to duplicate list me daal do
+            } else {
+                seen.add(n); // pehli baar mila to seen me daalo
+            }
+        }
+        
+        int sum = 0;
+        for (int n : seen) {
+            if (!dup.contains(n)) { 
+                sum += n;
+            }
+        }
+        
+        return sum;
     }
 }
-
