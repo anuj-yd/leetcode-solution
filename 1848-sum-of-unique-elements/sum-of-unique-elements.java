1a99ff84363276ca5670bb20d1101a6281040011
Anuj Yadav <150239759+anuj-yd@users.noreply.github.com>
2025-09-18 11:39:13 +0530

    Time: 1 ms (72.34%) | Memory: 41.8 MB (8.20%) - LeetSync

--- 1848-sum-of-unique-elements/sum-of-unique-elements.java
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.java
@@ -2,39 +2,27 @@ import java.util.*;
 
 class Solution {
     public int sumOfUnique(int[] nums) {
-        // Set<Integer> seen = new HashSet<>();
-        // Set<Integer> dup = new HashSet<>();
+        Set<Integer> seen = new HashSet<>();
+        Set<Integer> dup = new HashSet<>();
         
-        // for (int n : nums) {
-        //     if (seen.contains(n)) {
-        //         dup.add(n); // agar pehle se hai to duplicate list me daal do
-        //     } else {
-        //         seen.add(n); // pehli baar mila to seen me daalo
-        //     }
-        // }
-        
-        // int sum = 0;
-        // for (int n : seen) {
-        //     if (!dup.contains(n)) { 
-        //         sum += n;
-        //     }
-        // }
-        
-        // return sum;
-
-        int sum = 0;
-        Map<Integer, Integer> mp = new HashMap<>();
-
-        for (int i : nums) {
-            mp.put(i, mp.getOrDefault(i, 0) + 1);
-        }
-
-        for (Map.Entry<Integer, Integer> entry : mp.entrySet()) {
-            if (entry.getValue() == 1) {
-                sum += entry.getKey();
+        for (int n : nums) {
+            if (seen.contains(n)) {
+                dup.add(n); // agar pehle se hai to duplicate list me daal do
+            } else {
+                seen.add(n); // pehli baar mila to seen me daalo
             }
         }
-
+        
+        int sum = 0;
+        for (int n : seen) {
+            if (!dup.contains(n)) { 
+                sum += n;
+            }
+        }
+        
         return sum;
+
+        
+
     }
 }
