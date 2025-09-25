f62e355351d1d3965b27ba38a9d8763adb888db3
Anuj Yadav <150239759+anuj-yd@users.noreply.github.com>
2025-09-25 09:42:33 +0530

    Time: 1 ms (99.85%) | Memory: 46.1 MB (7.00%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,20 +1,42 @@
+// class Solution {
+//     public int minimumTotal(List<List<Integer>> triangle) {
+//         int n = triangle.size();
+//         int[] dp = new int[n];
+
+//         // initialize dp with last row
+//         for (int i = 0; i < n; i++) {
+//             dp[i] = triangle.get(n - 1).get(i);
+//         }
+
+//         // bottom-up DP
+//         for (int row = n - 2; row >= 0; row--) {
+//             for (int col = 0; col <= row; col++) {
+//                 dp[col] = Math.min(dp[col], dp[col + 1]) + triangle.get(row).get(col);
+//             }
+//         }
+
+//         return dp[0]; // top element now contains result
+//     }
+// }
+
 class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size();
-        int[] dp = new int[n];
-
-        // initialize dp with last row
-        for (int i = 0; i < n; i++) {
-            dp[i] = triangle.get(n - 1).get(i);
+    int m;
+    Integer[][] dp;
+    public int minimumTotal(List<List<Integer>> tran) {
+        m = tran.size();
+        dp = new Integer[m][m];
+       
+        return helper(tran,0,0);
+    }
+    int helper(List<List<Integer>> tran,int row, int col){
+        if(row==m){
+            return 0;
         }
-
-        // bottom-up DP
-        for (int row = n - 2; row >= 0; row--) {
-            for (int col = 0; col <= row; col++) {
-                dp[col] = Math.min(dp[col], dp[col + 1]) + triangle.get(row).get(col);
-            }
+        if(dp[row][col]!=null){
+            return dp[row][col];
         }
-
-        return dp[0]; // top element now contains result
+        int left = helper(tran,row+1,col);
+        int right = helper(tran,row+1,col+1);
+        return dp[row][col]=tran.get(row).get(col) + Math.min(left,right);
     }
 }
