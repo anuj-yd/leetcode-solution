239987084fa6592a668ea250b706a74c5bf4d3ea
Anuj Yadav <150239759+anuj-yd@users.noreply.github.com>
2025-10-08 23:00:29 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (28.34%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,21 +1,5 @@
 class Solution {
-    // public int min(int[] cost,int idx,int[] dp){
-    //     if(idx>=cost.length) return 0;
-    //     if(dp[idx]!=-1) return dp[idx];
-
-    //     dp[idx] = cost[idx] + Math.min(min(cost,idx+1,dp),min(cost,idx+2,dp));
-    //     return dp[idx];
-    // }
     public int minCostClimbingStairs(int[] cost) {
-
-        // int dp[] = new int[cost.length+1];
-        // Arrays.fill(dp,-1);
-
-        // return Math.min(min(cost,0,dp),min(cost,1,dp));
-
-        
-        // tabulation dp
-        // int n = cost.length;
         int n = cost.length;
 	    int[] dp = new int[n];
 	    for (int i=0; i<n; i++) {
@@ -23,7 +7,5 @@ class Solution {
 	    	else dp[i] = cost[i] + Math.min(dp[i-1], dp[i-2]);
 	    }
 	    return Math.min(dp[n-1], dp[n-2]);
-        // return (int)Math.min(dp[n-1],dp[n-2]);
-        
     }
 }
