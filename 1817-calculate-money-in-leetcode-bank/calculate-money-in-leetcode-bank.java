e553e015e829d30f5d86a22a245750f952da6d8f
Anuj Yadav <150239759+anuj-yd@users.noreply.github.com>
2025-10-25 21:20:20 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (15.30%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.java
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.java
@@ -1,17 +1,33 @@
+// class Solution {
+//     public int totalMoney(int n) {
+//         int mon = 1; 
+//         int d = 0;   
+//         int save = 0;
+
+//         while(d < n){
+//             save += mon + (d % 7); // money for the current day
+//             d++;
+//             if(d % 7 == 0){ 
+//                 mon++;
+//             }
+//         }
+
+//         return save;
+//     }
+// }
+
+
 class Solution {
     public int totalMoney(int n) {
-        int mon = 1; 
-        int d = 0;   
-        int save = 0;
+        int fullWeeks = n / 7;
+        int remainingDays = n % 7;
 
-        while(d < n){
-            save += mon + (d % 7); // money for the current day
-            d++;
-            if(d % 7 == 0){ 
-                mon++;
-            }
-        }
+        // Sum of all full weeks
+        int sumWeeks = fullWeeks * 28 + 7 * fullWeeks * (fullWeeks - 1) / 2;
 
-        return save;
+        // Sum of remaining days
+        int sumRemaining = remainingDays * (fullWeeks + 1 + fullWeeks + remainingDays) / 2;
+
+        return sumWeeks + sumRemaining;
     }
 }
