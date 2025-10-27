d4ca2037c08fae8b877abdfc843cf65e954c0cc2
Anuj Yadav <150239759+anuj-yd@users.noreply.github.com>
2025-10-27 22:49:26 +0530

    Time: 12 ms (53.73%) | Memory: 45.6 MB (16.12%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.java
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.java
@@ -1,34 +1,27 @@
+import java.util.*;
+
 class Solution {
     public int numberOfBeams(String[] bank) {
 
         List<Integer> ans = new ArrayList<>();
-        for(String str : bank){
-            int count = 0;
-            char ch[] = str.toCharArray();
-            for(int i=0;i<ch.length;i++){
-                if(ch[i]=='1'){
-                    count++;
-                }
-            }
-            if(count!=0) ans.add(count);
-        }
+        int count = 0;
 
-        //for(int i = 0;i<bank.length;i++){
-            // int num = Integer.parseInt(bank[i]);
-            // while(num!=0){
-            //     count+=num&1;
-            //     num=num>>1;
-            // }
-            // ans.add(count);
+        for (int i = 0; i < bank.length; i++) {
+            String row = bank[i];
             
-            // count = 0;
-        //}
+            for (char c : row.toCharArray()) {
+                if (c == '1') count++;
+            }
+
+            if (count > 0) ans.add(count);
+            count = 0;
+        }
 
         int pro = 0;
-        for(int i=1;i<ans.size();i++){
-            pro += (ans.get(i-1)*ans.get(i));
+        for (int i = 1; i < ans.size(); i++) {
+            pro += ans.get(i - 1) * ans.get(i);
         }
+
         return pro;
-        
     }
 }
