96ba97d72dd206adfb713ce1cb0668fdd85f3bb4
Anuj Yadav <150239759+anuj-yd@users.noreply.github.com>
2025-11-11 00:16:34 +0530

    Time: 127 ms (42.96%) | Memory: 123.5 MB (12.68%) - LeetSync

---
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+class Solution {
+    public int minOperations(int[] nums) {
+        Stack<Integer> st = new Stack<>();
+        int ops = 0;
+        for (int num : nums) {
+            if (num == 0){
+                st.clear();
+                continue;
+            }
+            // remove any values greater than current num
+            while (!st.isEmpty() && st.peek() > num) {
+                st.pop();
+            }
+            if (st.isEmpty() || st.peek() < num) {
+                st.push(num);
+                ops++; // each push corresponds to one operation
+            }    
+        }
+        return ops;
+    }
+}
