c5311cf16691e828fc2b15dba963588bbaa5fc26
Anuj Yadav <150239759+anuj-yd@users.noreply.github.com>
2025-11-11 00:20:02 +0530

    Time: 11 ms (100.00%) | Memory: 137.7 MB (5.63%) - LeetSync

--- 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.java
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.java
@@ -1,23 +1,42 @@
-import java.util.Stack;
+// import java.util.Stack;
+
+// class Solution {
+//     public int minOperations(int[] nums) {
+//         Stack<Integer> st = new Stack<>();
+//         int ops = 0;
+//         for (int num : nums) {
+//             if (num == 0){
+//                 st.clear();
+//                 continue;
+//             }
+//             // remove any values greater than current num
+//             while (!st.isEmpty() && st.peek() > num) {
+//                 st.pop();
+//             }
+//             if (st.isEmpty() || st.peek() < num) {
+//                 st.push(num);
+//                 ops++; // each push corresponds to one operation
+//             }    
+//         }
+//         return ops;
+//     }
+// }
+
 
 class Solution {
     public int minOperations(int[] nums) {
-        Stack<Integer> st = new Stack<>();
-        int ops = 0;
-        for (int num : nums) {
-            if (num == 0){
-                st.clear();
-                continue;
+        var stack = new int[nums.length + 1];
+        var top = 0;
+        var ans = 0;
+        for (var i = 0; i < nums.length; i++) {
+            while (stack[top] > nums[i]) {
+                top--;
+                ans++;
             }
-            // remove any values greater than current num
-            while (!st.isEmpty() && st.peek() > num) {
-                st.pop();
-            }
-            if (st.isEmpty() || st.peek() < num) {
-                st.push(num);
-                ops++; // each push corresponds to one operation
-            }    
+            if (stack[top] != nums[i])
+                stack[++top] = nums[i];
         }
-        return ops;
+        return ans + top;
     }
 }
+
