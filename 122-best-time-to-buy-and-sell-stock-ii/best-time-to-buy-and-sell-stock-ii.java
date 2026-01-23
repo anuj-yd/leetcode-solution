5dd7ad695c7343db1058f967078c0aeedced77e9
Anuj Yadav <anujyadav992241@gmail.com>
2026-01-23 20:24:07 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (53.02%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,32 +1,42 @@
 class Solution {
-    private int stock(int idx,int buy,int[] arr,int dp[][]){
-        int profit = 0;
-        int n = arr.length;
-        if(idx==n) return 0;
-        if(dp[idx][buy]!=-1) return dp[idx][buy];
-        if(buy==1){
-            int pick = profit-arr[idx]+stock(idx+1,0,arr,dp);
-            int skip = stock(idx+1,buy,arr,dp);
-            dp[idx][buy] = Math.max(pick,skip);
-        }else{
-            int pick = profit+arr[idx]+stock(idx+1,1,arr,dp);
-            int skip = stock(idx+1,buy,arr,dp);
-            dp[idx][buy] = Math.max(pick,skip);
+    // private int stock(int idx,int buy,int[] arr,int dp[][]){
+    //     int profit = 0;
+    //     int n = arr.length;
+    //     if(idx==n) return 0;
+    //     if(dp[idx][buy]!=-1) return dp[idx][buy];
+    //     if(buy==1){
+    //         int pick = profit-arr[idx]+stock(idx+1,0,arr,dp);
+    //         int skip = stock(idx+1,buy,arr,dp);
+    //         dp[idx][buy] = Math.max(pick,skip);
+    //     }else{
+    //         int pick = profit+arr[idx]+stock(idx+1,1,arr,dp);
+    //         int skip = stock(idx+1,buy,arr,dp);
+    //         dp[idx][buy] = Math.max(pick,skip);
 
-        }
-        return dp[idx][buy];
-    }
+    //     }
+    //     return dp[idx][buy];
+    // }
     public int maxProfit(int[] prices) {
-        int buy = 1;
+        // int buy = 1;
+        // int idx = 0;
+        // int n = prices.length;
+        // int dp[][] = new int[n][2];
+        // for(int i=0;i<n;i++){
+        //     Arrays.fill(dp[i].-1);
+        // }
+        // return stock(idx,buy,prices,dp);
+
+
+        // greedy approch
+
         int idx = 0;
-        int n = prices.length;
-        int dp[][] = new int[n][2];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<2;j++){
-                dp[i][j]=-1;
-            }
+        int ans = 0;
+
+        for(int i = 0;i<prices.length;i++){
+            if(prices[i]>prices[idx]) ans+=prices[i]-prices[idx];
+            idx=i;
         }
-        return stock(idx,buy,prices,dp);
+        return ans;
 
         
     }
