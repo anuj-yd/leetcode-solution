46d0aaa637887f2add86f7020ed7b42d1d00ed2e
Anuj Yadav <anujyadav992241@gmail.com>
2026-01-26 01:11:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (55.58%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.java
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -3,7 +3,7 @@ class Solution {
         int count = 0;
         while (n != 0) {
             count += (n & 1);
-            n >>>= 1;
+            n >>= 1;
         }
         return count;
     }
