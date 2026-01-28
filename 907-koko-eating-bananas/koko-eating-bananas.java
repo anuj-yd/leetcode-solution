4748b52780dac9e3a4793a087a9e4aad38e4de6a
Anuj Yadav <anujyadav992241@gmail.com>
2026-01-28 21:14:18 +0530

    Time: 20 ms (38.53%) | Memory: 47.4 MB (96.46%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,30 @@
+class Solution {
+    private long totalBananas(int[] piles,int mid){
+        long total = 0;
+        for(int n : piles){
+            total += (long)Math.ceil(n/(double)mid);
+        }
+        return total;
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        int max = Integer.MIN_VALUE;
+        for(int n : piles){
+            if(n>max) max = n;
+        }
+        int low = 1;
+        int high = max;
+        int ans = -1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            long total = totalBananas(piles,mid);
+            if(total<=h){
+                ans = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }   
+        }
+        return ans;
+        
+    }
+}
