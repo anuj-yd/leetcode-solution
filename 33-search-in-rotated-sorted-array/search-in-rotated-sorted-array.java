333b1f63622fc5c459a070d090fc1b90193e2d0f
Anuj Yadav <anujyadav992241@gmail.com>
2026-01-29 11:09:20 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (38.18%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -3,26 +3,29 @@ class Solution {
         int low = 0;
         int high = nums.length-1;
         while(low<=high){
-            int mid = low + (high-low)/2;
-            if(nums[mid]==target) return mid;
+            int mid = low+(high-low)/2;
 
-            if(nums[low]<=nums[mid]){
-                if(nums[low]<=target&&target<=nums[mid]){
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[low]<=nums[mid]){
+                if(nums[low]<=target&&target<nums[mid]){
                     high = mid-1;
                 }else{
                     low = mid+1;
-                }   
+                }
+
             }else{
-                if(nums[mid]<=target&&target<=nums[high]){
+                if(nums[mid]<target&&target<=nums[high]){
                     low = mid+1;
                 }else{
                     high = mid-1;
                 }
-
             }
-            
         }
         return -1;
+
+
         
     }
 }
