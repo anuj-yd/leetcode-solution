55934f5a8096b9e1c987a2615695a656bb8a87b9
Anuj Yadav <anujyadav992241@gmail.com>
2026-01-29 10:24:31 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (55.03%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -3,21 +3,54 @@ class Solution {
         int row = matrix.length;
         int col = matrix[0].length;
 
-        int low = 0;
-        int high = col*row-1;
-        while(low<=high){
-            int mid = low+(high-low)/2;
-            int middle = matrix[mid/col][mid%col];
-            if(middle==target){
-                return true;
-            }
-            else if(middle<=target){
-                low = mid+1;
-            }else{
-                high = mid-1;
+        for(int i=0;i<row;i++){
+            if(matrix[i][0]<=target&&target<=matrix[i][col-1]){
+                int low = 0;
+                int high = col-1;
+
+                while(low<=high){
+                    int mid = low+(high-low)/2;
+
+                    if(matrix[i][mid]==target){
+                        return true;
+                    }else if(matrix[i][mid]<target){
+                        low = mid+1;
+                    }else{
+                        high = mid-1;
+                    }
+
+                }
             }
         }
         return false;
+
+        
+
+
+
+
+
+
+
+
+
+        // int low = 0;
+        // int high = col*row-1;
+        // while(low<=high){
+        //     int mid = low+(high-low)/2;
+        //     int middle = matrix[mid/col][mid%col];
+        //     // rowIndex = mid / totalColumns
+        //     // colIndex = mid % totalColumns
+        //     if(middle==target){
+        //         return true;
+        //     }
+        //     else if(middle<=target){
+        //         low = mid+1;
+        //     }else{
+        //         high = mid-1;
+        //     }
+        // }
+        // return false;
         
     }
 }
