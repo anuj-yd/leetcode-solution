d3c525a307fe4cf0feabd3d6f242f99631127799
Anuj Yadav <anujyadav992241@gmail.com>
2026-01-29 10:13:09 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (17.74%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,41 +1,23 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target){
-        // log(m*n);
-        int n = matrix.length;
-        int m = matrix[0].length;
-        int s = 0;
-        int e = m*n-1;
-        while(s<=e){
-            int mid = s+(e-s)/2;
-            int midele = matrix[mid/m][mid%m];
-            if(midele == target){
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        int low = 0;
+        int high = col*row-1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            int middle = matrix[mid/col][mid%col];
+            if(middle==target){
                 return true;
-            }else if(target<midele){
-                e = mid-1;
+            }
+            else if(middle<=target){
+                low = mid+1;
             }else{
-                s = mid+1;
+                high = mid-1;
             }
         }
         return false;
-        
-
-        // m*n time complexity h
-        // int row = matrix.length;
-        // int col = matrix[0].length;
-
-        // int r = 0;
-        // int c = col-1;
-
-        // while(r<row && c>=0){
-        //     if(matrix[r][c]==target){
-        //         return true;
-        //     }else if(matrix[r][c]<target){
-        //         r++;
-        //     }else{
-        //         c--;
-        //     }
-        // }
-        // return false;
         
     }
 }
