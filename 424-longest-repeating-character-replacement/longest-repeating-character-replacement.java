a5505b183c1341e0177f19f6c4e6ef4256747318
Anuj Yadav <anujyadav992241@gmail.com>
2026-01-31 09:57:52 +0530

    Time: 6 ms (98.64%) | Memory: 46.4 MB (37.20%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,6 +1,7 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        Map<Character,Integer> map = new HashMap<>();
+        // Map<Character,Integer> map = new HashMap<>();
+        int freq[] = new int[26];
 
         int left = 0;
         int right = 0;
@@ -9,17 +10,27 @@ class Solution {
         int maxFreq = 0;
 
         while(right<s.length()){
-            char ch = s.charAt(right);
-            map.put(ch,map.getOrDefault(ch,0)+1);
-            maxFreq = Math.max(maxFreq, map.get(ch));
-            while((right-left+1)-maxFreq>k){
-                char ch2 = s.charAt(left);
-                map.put(ch2,map.get(ch2)-1);
-                if(map.get(ch2)==0) map.remove(ch2);
-                left = left+1;
+            // char ch = s.charAt(right);
+            // map.put(ch,map.getOrDefault(ch,0)+1);
+            // maxFreq = Math.max(maxFreq, map.get(ch));
+            // while((right-left+1)-maxFreq>k){
+            //     char ch2 = s.charAt(left);
+            //     map.put(ch2,map.get(ch2)-1);
+            //     if(map.get(ch2)==0) map.remove(ch2);
+            //     left = left+1;
+            // }
+            // maxLen=Math.max(maxLen,right-left+1);
+            // right++;
+            int idx = s.charAt(right) - 'A';
+            freq[idx]++;
+            maxFreq = Math.max(maxFreq, freq[idx]);
+            while ((right - left + 1) - maxFreq > k) {
+                freq[s.charAt(left) - 'A']--;
+                left++;
             }
             maxLen=Math.max(maxLen,right-left+1);
             right++;
+
         }
         return maxLen;
         
