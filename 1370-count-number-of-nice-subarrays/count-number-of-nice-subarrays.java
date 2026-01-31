b42bcb26f4fceb750364b8725e3860e8a54c9a0a
Anuj Yadav <anujyadav992241@gmail.com>
2026-01-31 12:57:18 +0530

    Time: 42 ms (25.22%) | Memory: 56.3 MB (88.06%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,30 +1,46 @@
 class Solution {
-    private int atMost(int[] nums,int k){
+    // private int atMost(int[] nums,int k){
+    //     int left = 0;
+    //     int right = 0;
+
+    //     int sum = 0;
+    //     int odd = 0;
+    //     int count = 0;
+    //     while(right<nums.length){
+    //        if(nums[right]%2!=0){
+    //             odd++;
+    //        }
+
+    //        while(odd>k){
+    //             if(nums[left]%2!=0){
+    //                 odd--;
+    //             }
+    //             left++;
+    //        }
+    //        count += right-left+1;
+    //        right++;
+    //     }
+    //     return count;
+    // }
+    public int numberOfSubarrays(int[] nums, int k) {
+        Map<Integer,Integer> map = new HashMap<>();
         int left = 0;
         int right = 0;
-
         int sum = 0;
-        int odd = 0;
         int count = 0;
+        map.put(0, 1);
         while(right<nums.length){
-           if(nums[right]%2!=0){
-                odd++;
-           }
+            sum+=nums[right]%2;
 
-           while(odd>k){
-                if(nums[left]%2!=0){
-                    odd--;
-                }
-                left++;
-           }
-           count += right-left+1;
-           right++;
+            if(map.containsKey(sum-k)){
+                count+=map.get(sum-k);
+            }
+            map.put(sum,map.getOrDefault(sum,0)+1);
+            right++;
+
         }
         return count;
-    }
-    public int numberOfSubarrays(int[] nums, int k) {
-        // Map<Integer,Integer> map = new HashMap<>();
-        return atMost(nums,k)-atMost(nums,k-1);
+        // return atMost(nums,k)-atMost(nums,k-1);
         
         
     }
