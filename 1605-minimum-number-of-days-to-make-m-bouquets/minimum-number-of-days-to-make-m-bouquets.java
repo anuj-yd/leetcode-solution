d8f151a8d56d56ec89347243df42f3f7929ef589
Anuj Yadav <anujyadav992241@gmail.com>
2026-02-02 01:18:47 +0530

    Time: 15 ms (96.49%) | Memory: 83 MB (8.56%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,52 +1,46 @@
 class Solution {
-    public boolean required(int mid, int[] arr, int m, int k) {
-        int flower = 0;
+    public boolean required(int day,int[] bloomDay,int m,int k){
+        int count = 0;
         int bouq = 0;
-        for (int n : arr) {
-            if (n <= mid) {
-                if (flower + 1 <k) {
-                    flower++;
-                }else {
-                    bouq++;
-                    flower = 0;
-                }
+        for(int n : bloomDay){
+            if(n<=day){
+                count++;
             }else{
-                flower=0;
+                bouq += (count/k);
+                count = 0;
             }
         }
-        return bouq >= m;
+        bouq += (count/k);
+        if(bouq>=m) return true;
+        return false;
     }
-
     public int minDays(int[] bloomDay, int m, int k) {
         int n = bloomDay.length;
-        long val = (long) m * k;
-        if (val > n)
-            return -1;
+        long val = (long)m*k;
+        if(val>n) return -1; 
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
-
-        for (int i = 0; i < bloomDay.length; i++) {
-            if (bloomDay[i] > max)
-                max = bloomDay[i];
-            if (bloomDay[i] < min)
-                min = bloomDay[i];
+        
+        for(int i=0;i<bloomDay.length;i++){
+            if(bloomDay[i]>max) max = bloomDay[i];
+            if(bloomDay[i]<min) min = bloomDay[i];
         }
 
         int low = min;
         int high = max;
         int ans = -1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            boolean req = required(mid, bloomDay, m, k);
-            if (req) {
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            boolean req = required(mid,bloomDay,m,k);
+            if(req){
                 ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+                high = mid-1;
+            }else{
+                low = mid+1;
             }
-
+            
         }
         return ans;
-
+        
     }
 }
