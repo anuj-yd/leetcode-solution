450563f93f29857a6bd3629101b5891c3afe7482
Anuj Yadav <anujyadav992241@gmail.com>
2026-02-07 10:32:54 +0530

    Time: 12 ms (81.37%) | Memory: 44.3 MB (30.62%) - LeetSync

---
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    
+    public void buildParent(TreeNode root,TreeNode prnt,Map<TreeNode,TreeNode> map){
+        if(root==null) return;
+
+        map.put(root,prnt);
+        buildParent(root.left,root,map);
+        buildParent(root.right,root,map);
+    }
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        Map<TreeNode, TreeNode> map = new HashMap<>(); // child->parent mapping to move upward
+        buildParent(root, null, map);
+        Queue<TreeNode> q = new LinkedList<>();
+        Set<TreeNode> visited = new HashSet<>();
+        q.offer(target);
+        visited.add(target);
+        int dist = 0;
+        while(!q.isEmpty()){
+            int size = q.size();
+            if(dist==k){
+                List<Integer> ans = new ArrayList<>();
+                for(TreeNode node : q){
+                    ans.add(node.val);
+                }
+                return ans;
+            }
+
+            for(int i=0;i<size;i++){
+                TreeNode temp = q.poll();
+
+                if(temp.left!=null && !visited.contains(temp.left)){
+                    q.offer(temp.left);
+                    visited.add(temp.left);
+
+                }
+                if(temp.right!=null && !visited.contains(temp.right)){
+                    q.offer(temp.right);
+                    visited.add(temp.right);
+                }
+
+                TreeNode parent = map.get(temp);
+                if(parent != null && !visited.contains(parent)){
+                    visited.add(parent);
+                    q.offer(parent);
+                }
+            }
+            dist++;
+        }
+        return new ArrayList<>();
+    }
+}
