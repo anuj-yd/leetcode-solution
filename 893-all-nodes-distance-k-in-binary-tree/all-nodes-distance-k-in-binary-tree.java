c213f92442c812b1d60de70d523c94b63324f4d3
Anuj Yadav <anujyadav992241@gmail.com>
2026-02-07 10:40:20 +0530

    Time: 13 ms (47.48%) | Memory: 44.3 MB (45.25%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -9,16 +9,42 @@
  */
 class Solution {
     
-    public void buildParent(TreeNode root,TreeNode prnt,Map<TreeNode,TreeNode> map){
-        if(root==null) return;
+    // public void buildParent(TreeNode root,TreeNode prnt,Map<TreeNode,TreeNode> map){
+    //     if(root==null) return;
 
-        map.put(root,prnt);
-        buildParent(root.left,root,map);
-        buildParent(root.right,root,map);
+    //     map.put(root,prnt);
+    //     buildParent(root.left,root,map);
+    //     buildParent(root.right,root,map);
+    // }
+
+    public void buildParent(TreeNode root, Map<TreeNode, TreeNode> map){
+
+        if(root == null) return;
+
+        Queue<TreeNode> q = new LinkedList<>();
+
+        q.offer(root);
+        map.put(root, null); // root has no parent
+
+        while(!q.isEmpty()){
+
+            TreeNode curr = q.poll();
+
+            if(curr.left != null){
+                map.put(curr.left, curr); // child -> parent
+                q.offer(curr.left);
+            }
+
+            if(curr.right != null){
+                map.put(curr.right, curr); // child -> parent
+                q.offer(curr.right);
+            }
+        }
     }
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
         Map<TreeNode, TreeNode> map = new HashMap<>(); // child->parent mapping to move upward
-        buildParent(root, null, map);
+        // buildParent(root, null, map);
+        buildParent(root, map);
         Queue<TreeNode> q = new LinkedList<>();
         Set<TreeNode> visited = new HashSet<>();
         q.offer(target);
