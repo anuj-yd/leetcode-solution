a92182b66bfe4291d5b2c23bf4d6250b44e1bf06
Anuj Yadav <anujyadav992241@gmail.com>
2026-02-09 12:46:08 +0530

    Time: 0 ms (100.00%) | Memory: 49.2 MB (91.87%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -14,31 +14,43 @@
  * }
  */
 class Solution {
-    private int rightH(TreeNode root){
-        int count = 0;
-        while(root.right!=null){
-            count++;
-            root = root.right;
-        }
-        return count;
+    // private int rightH(TreeNode root){
+    //     int count = 0;
+    //     while(root.right!=null){
+    //         count++;
+    //         root = root.right;
+    //     }
+    //     return count;
         
-    }
-    private int leftH(TreeNode root){
-        int count = 0;
-        while(root.left!=null){
-            count++;
-            root = root.left;
-        }
-        return count;
+    // }
+    // private int leftH(TreeNode root){
+    //     int count = 0;
+    //     while(root.left!=null){
+    //         count++;
+    //         root = root.left;
+    //     }
+    //     return count;
 
-    }
+    // }
     public int countNodes(TreeNode root) {
-        if(root == null) return 0;
-        int lh = leftH(root);
-        int rh = rightH(root);
+        // if(root == null) return 0;
+        // int lh = leftH(root);
+        // int rh = rightH(root);
 
-        if(lh==rh) return (2<<(lh))-1;
-        return 1+countNodes(root.left)+countNodes(root.right);
+        // if(lh==rh) return (2<<(lh))-1;
+        // return 1+countNodes(root.left)+countNodes(root.right);
+        
+        if(root == null) return 0;
+        int count = 0;
+        if(root.left == null && root.right == null) return 1;
+        if(root.left != null){
+            count++;
+        }
+        else if(root.right != null){
+            count++;
+        }
+        return count+countNodes(root.left)+countNodes(root.right);
+        
 
     }
 }
