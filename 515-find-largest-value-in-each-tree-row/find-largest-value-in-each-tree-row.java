b7a81ea0886c7afbe6843105eddc158336973712
Anuj Yadav <anujyadav992241@gmail.com>
2026-03-07 23:42:27 +0530

    Time: 1 ms (98.54%) | Memory: 46.6 MB (89.80%) - LeetSync

--- 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.java
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.java
@@ -14,28 +14,41 @@
  * }
  */
 class Solution {
+    private void solve(List<Integer> ans,int lvl,TreeNode root){
+        if(root == null) return;
+        if(ans.size()==lvl){
+            ans.add(root.val);
+        }else{
+            ans.set(lvl,Math.max(ans.get(lvl),root.val));
+        }
+        if(root.left!=null) solve(ans,lvl+1,root.left);
+        if(root.right!=null) solve(ans,lvl+1,root.right);
+
+    }
     public List<Integer> largestValues(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        if(root==null) return ans;
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-        while(!q.isEmpty()){
-            int len = q.size();
-            int max = Integer.MIN_VALUE;
-            for(int i = 0;i<len;i++){
-
-                TreeNode temp = q.poll();
-
-                if(temp.val>max){
-                    max = temp.val;
-                }
-                if(temp.left!=null) q.offer(temp.left);
-                if(temp.right!=null) q.offer(temp.right);
-
-            } 
-            ans.add(max);
-        }
+        solve(ans,0,root);
         return ans;
+        // if(root==null) return ans;
+        // Queue<TreeNode> q = new LinkedList<>();
+        // q.offer(root);
+        // while(!q.isEmpty()){
+        //     int len = q.size();
+        //     int max = Integer.MIN_VALUE;
+        //     for(int i = 0;i<len;i++){
+
+        //         TreeNode temp = q.poll();
+
+        //         if(temp.val>max){
+        //             max = temp.val;
+        //         }
+        //         if(temp.left!=null) q.offer(temp.left);
+        //         if(temp.right!=null) q.offer(temp.right);
+
+        //     } 
+        //     ans.add(max);
+        // }
+        // return ans;
         
     }
 }
