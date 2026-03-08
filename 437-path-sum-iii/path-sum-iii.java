e2280ca6968c2f4ad12504c715a816b0e1e59930
Anuj Yadav <anujyadav992241@gmail.com>
2026-03-08 23:06:14 +0530

    Time: 3 ms (97.66%) | Memory: 46.4 MB (46.29%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.java
+++ 437-path-sum-iii/path-sum-iii.java
@@ -1,32 +1,26 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public int countPath(TreeNode root, long targetSum){
+
+    public int dfs(TreeNode root, long currSum, int target, HashMap<Long,Integer> map){
         if(root == null) return 0;
-        int count = 0;
 
-        if(root.val == targetSum) count++;
+        currSum += root.val;
 
-        count += countPath(root.left,targetSum-root.val);
-        count += countPath(root.right,targetSum-root.val);
+        int count = map.getOrDefault(currSum - target, 0);
+
+        map.put(currSum, map.getOrDefault(currSum,0) + 1);
+
+        count += dfs(root.left, currSum, target, map);
+        count += dfs(root.right, currSum, target, map);
+
+        map.put(currSum, map.get(currSum) - 1); // backtrack
+
         return count;
     }
+
     public int pathSum(TreeNode root, int targetSum) {
-        if(root==null) return 0;
-        int ans = countPath(root,targetSum);
-        return ans + pathSum(root.left,targetSum)+pathSum(root.right,targetSum);
+        HashMap<Long,Integer> map = new HashMap<>();
+        map.put(0L,1);
+
+        return dfs(root, 0, targetSum, map);
     }
 }
