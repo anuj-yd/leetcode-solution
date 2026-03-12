85f1250f678f5d401337fe19b96ff767bfcadd5e
Anuj Yadav <anujyadav992241@gmail.com>
2026-03-13 01:37:34 +0530

    Time: 79 ms (31.24%) | Memory: 75 MB (23.02%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,40 +1,28 @@
 class Solution {
-    // private boolean solve(int[] nums,int idx,int sum,Boolean[][] dp){
-    //     if(sum==0) return true;
-    //     if(idx==nums.length) return false;
-    //     boolean take = false;
-    //     if(nums[idx]<=sum){
-    //         take = solve(nums,idx+1,sum-nums[idx],dp);
-    //     }
-    //     boolean skip = solve(nums,idx+1,sum,dp);
-    //     return dp[idx][sum] = take || skip;
-    // }
+    public boolean check(int[] nums,Boolean[][] dp,int sum,int idx){
+        if(idx==0) return false;
+        if(sum==0) return true;
+
+         if(dp[idx][sum] != null) return dp[idx][sum];
+
+        boolean pick = false;
+
+        if(nums[idx-1]<=sum){
+            pick = check(nums,dp,sum-nums[idx-1],idx-1);
+            
+        }
+        boolean skip = check(nums,dp,sum,idx-1);
+        return dp[idx][sum] = pick || skip;
+
+    }
     public boolean canPartition(int[] nums) {
+        int n = nums.length;
         int sum = 0;
-        for(int n : nums) sum+=n;
-        boolean ans = false;
+        for(int num : nums) sum+=num;
+        Boolean[][] dp = new Boolean[n+1][sum/2 + 1];
         if(sum%2!=0) return false;
-        Boolean dp[][] = new Boolean[nums.length+1][sum/2+1];
-        for(int i=0;i<=nums.length;i++){
-            for(int j=0;j<=sum/2;j++){
-                if(i==0) dp[i][j] = false;
-                if(j==0) dp[i][j] = true;
-            }
-        }
+        return check(nums,dp,sum/2,n);
 
-        for(int i=1;i<=nums.length;i++){
-            for(int j=1;j<=sum/2;j++){
-                if(nums[i-1]<=j){
-                    dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
-                }else{
-
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
-
-        return dp[nums.length][sum/2];
-
-        // return solve(nums,0,sum/2,dp);
+        
     }
 }
