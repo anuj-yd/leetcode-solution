1edc9025d51e2e7796a0c4cf2378d7f7433fc74b
Anuj Yadav <anujyadav992241@gmail.com>
2026-03-13 14:35:15 +0530

    Time: 30 ms (82.14%) | Memory: 43.6 MB (96.68%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,4 +1,31 @@
 class Solution {
+    // 1d
+    public boolean canPartition(int[] nums) {
+
+        int sum = 0;
+
+        for(int num : nums){
+            sum += num;
+        }
+
+        if(sum % 2 != 0) return false;
+
+        int target = sum / 2;
+
+        boolean[] dp = new boolean[target + 1];
+
+        dp[0] = true;
+
+        for(int num : nums){
+            for(int j = target; j >= num; j--){
+                dp[j] = dp[j] || dp[j - num];
+            }
+        }
+
+        return dp[target];
+    }
+}
+
     // public boolean check(int[] nums,Boolean[][] dp,int sum,int idx){
     //     if(idx==0) return false;
     //     if(sum==0) return true;
@@ -15,33 +42,30 @@ class Solution {
     //     return dp[idx][sum] = pick || skip;
 
     // }
-    public boolean canPartition(int[] nums) {
-        int n = nums.length;
-        int sum = 0;
-        for(int num : nums) sum+=num;
-        Boolean[][] dp = new Boolean[n+1][sum/2 + 1];
-        if(sum%2!=0) return false;
+    // 2d dp
+    // public boolean canPartition(int[] nums) {
+    //     int n = nums.length;
+    //     int sum = 0;
+    //     for(int num : nums) sum+=num;
+    //     Boolean[][] dp = new Boolean[n+1][sum/2 + 1];
+    //     if(sum%2!=0) return false;
 
-        for(int i = 0; i <= n; i++){
-            dp[i][0] = true;
-        }
+    //     for(int i = 0; i <= n; i++){
+    //         dp[i][0] = true;
+    //     }
 
-        for(int j = 1; j <= sum/2; j++){
-            dp[0][j] = false;
-        }
+    //     for(int j = 1; j <= sum/2; j++){
+    //         dp[0][j] = false;
+    //     }
 
-        for(int i=1;i<n+1;i++){
-            for(int j = 1;j<sum/2+1;j++){
-                if(nums[i-1]<=j){
-                    dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
-                }else{
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
-        return dp[n][sum/2];
+    //     for(int i=1;i<n+1;i++){
+    //         for(int j = 1;j<sum/2+1;j++){
+    //             if(nums[i-1]<=j){
+    //                 dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
+    //             }else{
+    //                 dp[i][j] = dp[i-1][j];
+    //             }
+    //         }
+    //     }
+    //     return dp[n][sum/2];
         // return check(nums,dp,sum/2,n);
-
-        
-    }
-}
