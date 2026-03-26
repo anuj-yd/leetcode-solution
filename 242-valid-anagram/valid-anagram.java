9bdc2bb2e85929c45f7fbe5ec9f446f7bd855a00
Anuj Yadav <anujyadav992241@gmail.com>
2026-03-27 01:16:11 +0530

    Time: 5 ms (60.44%) | Memory: 44.6 MB (63.54%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,31 +1,65 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 
-        Map<Character,Integer> map = new HashMap<>();
+        int[] freq = new int[26];
 
         for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
-             
-            if(map.containsKey(ch)){
-                map.put(ch,map.getOrDefault(ch,0)+1);
-            }else{
-                map.put(ch,1);
-            }
 
+            if(freq[ch - 'a'] != 0){
+                freq[ch - 'a'] = freq[ch - 'a'] + 1;
+            } else {
+                freq[ch - 'a'] = 1;
+            }
         }
 
         for(int i=0;i<t.length();i++){
             char ch = t.charAt(i);
-            if(map.containsKey(ch)){
-                map.put(ch,map.getOrDefault(ch,0)-1);
-                if(map.get(ch)==0) map.remove(ch);
-            }else{
+
+            if(freq[ch - 'a'] != 0){
+                freq[ch - 'a'] = freq[ch - 'a'] - 1;
+            } else {
                 return false;
             }
-            
         }
 
-        return map.size() == 0;
-        
+        for(int i=0;i<26;i++){
+            if(freq[i] != 0){
+                return false;
+            }
+        }
+
+        return true;
     }
 }
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+
+//         Map<Character,Integer> map = new HashMap<>();
+
+//         for(int i=0;i<s.length();i++){
+//             char ch = s.charAt(i);
+             
+//             if(map.containsKey(ch)){
+//                 map.put(ch,map.getOrDefault(ch,0)+1);
+//             }else{
+//                 map.put(ch,1);
+//             }
+
+//         }
+
+//         for(int i=0;i<t.length();i++){
+//             char ch = t.charAt(i);
+//             if(map.containsKey(ch)){
+//                 map.put(ch,map.getOrDefault(ch,0)-1);
+//                 if(map.get(ch)==0) map.remove(ch);
+//             }else{
+//                 return false;
+//             }
+            
+//         }
+
+//         return map.size() == 0;
+        
+//     }
+// }
