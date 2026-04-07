2bb9c243f2a799518bfaa364e85ddbee15625ea2
Anuj Yadav <anujyadav992241@gmail.com>
2026-04-08 00:50:58 +0530

    Time: 6 ms (43.44%) | Memory: 44.3 MB (77.91%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,65 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
 
         int[] freq = new int[26];
 
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-
-            if(freq[ch - 'a'] != 0){
-                freq[ch - 'a'] = freq[ch - 'a'] + 1;
-            } else {
-                freq[ch - 'a'] = 1;
-            }
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
         }
 
-        for(int i=0;i<t.length();i++){
-            char ch = t.charAt(i);
-
-            if(freq[ch - 'a'] != 0){
-                freq[ch - 'a'] = freq[ch - 'a'] - 1;
-            } else {
-                return false;
-            }
-        }
-
-        for(int i=0;i<26;i++){
-            if(freq[i] != 0){
-                return false;
-            }
+        for (int count : freq) {
+            if (count != 0) return false;
         }
 
         return true;
     }
 }
-// class Solution {
-//     public boolean isAnagram(String s, String t) {
-
-//         Map<Character,Integer> map = new HashMap<>();
-
-//         for(int i=0;i<s.length();i++){
-//             char ch = s.charAt(i);
-             
-//             if(map.containsKey(ch)){
-//                 map.put(ch,map.getOrDefault(ch,0)+1);
-//             }else{
-//                 map.put(ch,1);
-//             }
-
-//         }
-
-//         for(int i=0;i<t.length();i++){
-//             char ch = t.charAt(i);
-//             if(map.containsKey(ch)){
-//                 map.put(ch,map.getOrDefault(ch,0)-1);
-//                 if(map.get(ch)==0) map.remove(ch);
-//             }else{
-//                 return false;
-//             }
-            
-//         }
-
-//         return map.size() == 0;
-        
-//     }
-// }
