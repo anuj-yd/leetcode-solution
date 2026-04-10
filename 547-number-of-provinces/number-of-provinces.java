68649b9a85648c73c4ddd6c5cf79d18f19f3a398
Anuj Yadav <anujyadav992241@gmail.com>
2026-04-10 15:28:21 +0530

    Time: 4 ms (17.83%) | Memory: 49.2 MB (17.99%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,25 +1,33 @@
 class Solution {
-    public int findCircleNum(int[][] isConnected){
-        int n = isConnected.length;
-        boolean[] visited = new boolean[n];
-        int count = 0; 
-
-        for(int i=0;i<n;i++){
+    public void dfs(int i,List<List<Integer>> graph,boolean[] visited){
+        visited[i] = true;
+        for(int nbr : graph.get(i)){
+            if(!visited[nbr]){
+                dfs(nbr,graph,visited);
+            }
+        }
+    }
+    public int findCircleNum(int[][] isConnected) {
+        List<List<Integer>> graph = new ArrayList<>();
+        
+        int v = isConnected.length;
+        for(int i=0;i<v;i++) graph.add(new ArrayList<>());
+        for(int i=0;i<v;i++){
+            for(int j = 0;j<isConnected[0].length;j++){
+                if(isConnected[i][j]==1 && i!=j){
+                    graph.get(i).add(j);
+                    graph.get(j).add(i);
+                }   
+            }
+        }
+        boolean visited[] = new boolean[v];
+        int count=0;
+        for(int i=0;i<v;i++){
             if(!visited[i]){
                 count++;
-                dfs(i,isConnected,visited);
+                dfs(i,graph,visited);
             }
         }
         return count;
-        
-    }
-    public static void dfs(int node,int[][] graph,boolean[] visited){
-        visited[node]=true;
-        for(int j=0;j<graph.length;j++){
-            if(graph[node][j]==1 && !visited[j]){
-                dfs(j,graph,visited);
-            }
-        }
-
     }
 }
