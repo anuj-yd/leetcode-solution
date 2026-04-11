bf4616862d015563ae15d0d6ff7792454a18f2a3
Anuj Yadav <anujyadav992241@gmail.com>
2026-04-11 23:08:04 +0530

    Time: 4 ms (43.52%) | Memory: 52 MB (76.97%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,29 +1,34 @@
 class Solution {
-    public void dfs(int i,int j,char[][] grid){
+    private void dfs(int i,int j,char[][] grid,boolean[][] visited,int[][] direction){
         int n = grid.length;
         int m = grid[0].length;
+        visited[i][j] = true;
+        for(int[] dir : direction){
+            int ni = dir[0]+i;
+            int nj = dir[1]+j;
 
-        if(i<0 || j<0 || i>=n || j>=m || grid[i][j] == '0') return;
-        grid[i][j] = '0';
-        dfs(i+1,j,grid);
-        dfs(i-1,j,grid);
-        dfs(i,j+1,grid);
-        dfs(i,j-1,grid);
+            if(ni>=0 && nj>=0 && ni<n && nj<m && grid[ni][nj]=='1' && !visited[ni][nj]){
+                dfs(ni,nj,grid,visited,direction);
+            }
+        }
 
     }
     public int numIslands(char[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
+        boolean visited[][] = new boolean[n][m];
+        int[][] direction = {{1,0},{0,1},{-1,0},{0,-1}};
+
         int count = 0;
+
         for(int i=0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(grid[i][j]=='1'){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1' && !visited[i][j]){
+                    dfs(i,j,grid,visited,direction);
                     count++;
-                    dfs(i,j,grid);
                 }
             }
         }
         return count;
-        
     }
 }
