a6ad4e34f339d3dd33087e8be47acd2342ea7eb7
Anuj Yadav <anujyadav992241@gmail.com>
2026-04-14 02:16:47 +0530

    Time: 5 ms (15.68%) | Memory: 44.9 MB (76.87%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,32 +1,29 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Stack<Integer> st = new Stack<>();
-        int ans[] = new int[nums2.length];
-        Arrays.fill(ans,-1);
         int n = nums2.length;
-        for(int i=n-1;i>=0;i--){
-            while(st.size()>0 && nums2[i]>=st.peek()){
+        int nxg[] = new int[n];
+        Stack<Integer> st = new Stack<>();
+        nxg[n-1] = -1;
+        st.push(nums2[n-1]);
+        for(int i=n-2;i>=0;i--){
+            while(!st.isEmpty() && nums2[i]>=st.peek()){
                 st.pop();
             }
-            if(st.size()>0){
-                ans[i] = st.peek();
-
-            }
+            if(st.isEmpty()) nxg[i] = -1;
+            else nxg[i] = st.peek();
             st.push(nums2[i]);
         }
+        for(int i = 0;i<nums1.length;i++){
 
-        HashMap<Integer,Integer> h = new HashMap<>();
-        for(int i=0;i<nums2.length;i++){
-            h.put(nums2[i],ans[i]);
+            for(int j=0;j<nums2.length;j++){
+
+                if(nums1[i]==nums2[j]){
+                    nums1[i] = nxg[j];
+                    break;
+                }
+            }
+            
         }
-        int arr[] = new int[nums1.length];
-        for(int i=0;i<nums1.length;i++){
-            arr[i] = h.get(nums1[i]);
-        }
-
-        return arr;
-
-        
-
+        return nums1;
     }
 }
