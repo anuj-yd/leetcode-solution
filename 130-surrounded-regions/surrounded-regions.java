e68fab7d83f8573b580910033c5c4735a81171fb
Anuj Yadav <anujyadav992241@gmail.com>
2026-04-16 16:15:26 +0530

    Time: 2 ms (78.13%) | Memory: 47.8 MB (61.30%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,11 +1,19 @@
 class Solution {
-    static class Pair{
-        int nj;
-        int ni;
-        public Pair(int ni,int nj){
-            this.ni = ni;
-            this.nj = nj;
+
+    public void dfs(int i, int j, char[][] board, int row, int col) {
+        // boundary + valid check
+        if(i < 0 || j < 0 || i >= row || j >= col || board[i][j] != 'O') {
+            return;
         }
+
+        // mark safe
+        board[i][j] = '#';
+
+        // 4 directions
+        dfs(i+1, j, board, row, col);
+        dfs(i-1, j, board, row, col);
+        dfs(i, j+1, board, row, col);
+        dfs(i, j-1, board, row, col);
     }
 
     public void solve(char[][] board) {
@@ -13,47 +21,22 @@ class Solution {
         int row = board.length;
         int col = board[0].length;
 
-        Queue<Pair> q = new LinkedList<>();
-        boolean visited[][] = new boolean[row][col];
-
-        // 🔥 CHANGE 1: sirf boundary 'O'
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
+        // 🔥 Step 1: boundary se DFS start
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
                 if((i==0 || j==0 || i==row-1 || j==col-1) && board[i][j]=='O'){
-                    q.offer(new Pair(i,j));
-                    visited[i][j] = true;
-                    board[i][j] = '#'; // mark safe
-                }
-            }
-        } 
-
-        int dir[][] = {{1,0},{0,1},{-1,0},{0,-1}};
-
-        while(!q.isEmpty()){
-            Pair p = q.poll();
-
-            for(int[] d : dir){
-                int ni = p.ni+d[0];
-                int nj = p.nj+d[1];
-
-                // 🔥 CHANGE 2: boundary check pehle + mark '#'
-                if(ni>=0 && nj>=0 && ni<row && nj<col 
-                   && !visited[ni][nj] && board[ni][nj]=='O'){
-
-                    q.offer(new Pair(ni,nj));
-                    visited[ni][nj] = true;
-                    board[ni][nj] = '#'; // safe
+                    dfs(i, j, board, row, col);
                 }
             }
         }
 
-        // 🔥 CHANGE 3: final conversion
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(board[i][j]=='O'){
-                    board[i][j]='X';
-                } else if(board[i][j]=='#'){
-                    board[i][j]='O';
+        // 🔥 Step 2: final conversion
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(board[i][j] == 'O'){
+                    board[i][j] = 'X';
+                } else if(board[i][j] == '#'){
+                    board[i][j] = 'O';
                 }
             }
         }
