82a72e79af895a63e715b19dea17ad72726da531
Anuj Yadav <anujyadav992241@gmail.com>
2026-04-16 16:48:08 +0530

    Time: 8 ms (70.37%) | Memory: 60.1 MB (54.95%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,44 +1,29 @@
 class Solution {
-    static class Pair{
-        int ni;
-        int nj;
+    private void dfs(int i,int j,int grid[][],boolean visited[][]){
+        int row = grid.length;
+        int col = grid[0].length;
+        if(i<0 || j<0 || i>=row || j>=col || grid[i][j]==0 || visited[i][j]) return;
+        visited[i][j] = true;
 
-        public Pair(int ni,int nj){
-            this.ni = ni;
-            this.nj = nj;
-        }
+        dfs(i+1,j,grid,visited);
+        dfs(i,j+1,grid,visited);
+        dfs(i-1,j,grid,visited);
+        dfs(i,j-1,grid,visited);
+
     }
+
     public int numEnclaves(int[][] grid) {
         int row = grid.length;
         int col = grid[0].length;
-        Queue<Pair> q = new LinkedList<>();
         boolean visited[][] = new boolean[row][col];
-
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                 if((i==0||j==0||i==row-1||j==col-1)&& grid[i][j]==1){
-                    
-                    q.offer(new Pair(i,j));
-                    visited[i][j]=true;
-                    
+                    dfs(i,j,grid,visited); 
                 }
             }
         }
         int count = 0;
-        int dir[][] = {{1,0},{0,1},{-1,0},{0,-1}};
-        while(!q.isEmpty()){
-            Pair p = q.poll();          
-            for(int[] d : dir){
-                int ni = p.ni+d[0];
-                int nj = p.nj+d[1];         
-                if(ni>=0&&nj>=0&&ni<row
-                &&nj<col&&grid[ni][nj]==1&&!visited[ni][nj]){           
-                    q.offer(new Pair(ni,nj));
-                    visited[ni][nj]=true;    
-                }
-            }
-        }
-
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                 if(grid[i][j]==1 && !visited[i][j]){
