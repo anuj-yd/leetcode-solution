56ec5d25cb18ef5d50e66c465e0e6e8db1661af8
Anuj Yadav <anujyadav992241@gmail.com>
2026-05-02 03:47:55 +0530

    Time: 29 ms (93.03%) | Memory: 60.3 MB (11.79%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,39 +1,43 @@
 class Solution {
-    public static void solve(int[] nums,int target,int i,int j,List<List<Integer>> ans){
-        while(i<j){
-            int sum = nums[i]+nums[j];
-            if(sum<target){
+
+    public static void solve(int[] nums,int i,int j,
+                             List<List<Integer>> ans,int first){
+        while(i < j){
+            int sum = first + nums[i] + nums[j];
+
+            if(sum < 0){
                 i++;
-            }else if(sum>target){
+            }else if(sum > 0){
                 j--;
             }else{
-                while(i<j && nums[i]==nums[i+1]) i++;
-                while(i<j && nums[j]==nums[j-1]) j--;
+
+                while(i < j && nums[i] == nums[i+1]) i++;
+                while(i < j && nums[j] == nums[j-1]) j--;
+
                 List<Integer> temp = new ArrayList<>();
-                temp.add(-target);
+                temp.add(first);
                 temp.add(nums[i]);
                 temp.add(nums[j]);
                 ans.add(temp);
+
                 i++;
                 j--;
             }
         }
     }
+
     public List<List<Integer>> threeSum(int[] nums) {
         int n = nums.length;
         List<List<Integer>> ans = new ArrayList<>();
-        if(n<3) return ans;
+        if(n < 3) return ans;
+
         Arrays.sort(nums);
 
-        for(int i=0;i<n;i++){
-            if(i>0 && nums[i]==nums[i-1]){
-                continue;
-            }
-            int n1 = nums[i];
-            int target = -n1;
-
-            solve(nums,target,i+1,n-1,ans);
+        for(int i = 0; i < n; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            solve(nums, i+1, n-1, ans, nums[i]);
         }
+
         return ans;
     }
 }
