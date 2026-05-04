0e8c90a4fc1a669309b12fc21def82ab7bab9451
Anuj Yadav <anujyadav992241@gmail.com>
2026-05-04 14:05:20 +0530

    Time: 29 ms (22.53%) | Memory: 249.4 MB (86.69%) - LeetSync

--- 4171-rotate-non-negative-elements/rotate-non-negative-elements.java
+++ 4171-rotate-non-negative-elements/rotate-non-negative-elements.java
@@ -1,25 +1,29 @@
 class Solution {
     public int[] rotateElements(int[] nums, int k) {
         int n = nums.length;  
-    
         Deque<Integer> q = new ArrayDeque<>();
 
+        // collect non-negative
         for(int num : nums){
             if(num >= 0) q.addLast(num);
         }
-        if(q.size()==0) return nums;
-        k = k%q.size();
-        while(k!=0){
+
+        if(q.size() == 0) return nums;
+
+        k = k % q.size();
+
+        // rotate
+        for(int i = 0; i < k; i++){
             q.addLast(q.pollFirst());
-            k--;
         }
 
-        for(int i=0;i<n;i++){
-            if(nums[i]>=0){
-                nums[i] = q.poll();
+        // put back
+        for(int i = 0; i < n; i++){
+            if(nums[i] >= 0){
+                nums[i] = q.pollFirst();
             }
         }
+
         return nums;
-        
     }
 }
