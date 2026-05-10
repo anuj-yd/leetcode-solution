b986604073fe0810063cab7101a855200ec38e91
Anuj Yadav <anujyadav992241@gmail.com>
2026-05-10 16:24:36 +0530

    Time: 13 ms (92.26%) | Memory: 46.8 MB (75.00%) - LeetSync

--- 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.java
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.java
@@ -1,43 +1,31 @@
 class Solution {
 
-    int[][] dp;
-
-    public int solve(int[] nums, int tar, int idx, int j) {
-
-        int n = nums.length;
-
-        if (idx == n-1) return 0;
-
-        if (j >= n) return Integer.MIN_VALUE;
-
-        if (dp[idx][j] != -1) return dp[idx][j];
-
-        int pick = Integer.MIN_VALUE;
-
-        int diff = nums[j] - nums[idx];
-        if (-tar <= diff && diff <= tar) {
-            int next = solve(nums, tar, j, j + 1);
-            if (next != Integer.MIN_VALUE) {
-                pick = 1 + next;
-            }
-        }
-        int skip = solve(nums, tar, idx, j + 1);
-
-        return dp[idx][j] = Math.max(pick, skip);
-    }
-
     public int maximumJumps(int[] nums, int target) {
 
         int n = nums.length;
 
-        dp = new int[n][n];
+        int[] dp = new int[n];
 
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
+        Arrays.fill(dp, -1);
+
+        dp[0] = 0;
+
+        for (int i = 1; i < n; i++) {
+
+            for (int j = 0; j < i; j++) {
+
+                int diff = nums[i] - nums[j];
+
+                if (-target <= diff &&
+                    diff <= target &&
+                    dp[j] != -1) {
+
+                    dp[i] = Math.max(dp[i],
+                                     dp[j] + 1);
+                }
+            }
         }
 
-        int ans = solve(nums, target, 0, 1);
-
-        return ans == Integer.MIN_VALUE ? -1 : ans;
+        return dp[n - 1];
     }
 }
