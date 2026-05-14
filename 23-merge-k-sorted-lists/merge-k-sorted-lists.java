d0f3f428d3dee62e392545e62a8a3acb106e16b9
Anuj Yadav <anujyadav992241@gmail.com>
2026-05-14 19:39:33 +0530

    Time: 84 ms (8.35%) | Memory: 46.9 MB (45.52%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -9,53 +9,39 @@
  * }
  */
 class Solution {
-    public ListNode merge(ListNode list1,ListNode list2){
-        if(list1==null) return list2;
-        if(list2==null) return list1;
+    public ListNode merge(ListNode l1,ListNode l2){
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+
         ListNode dummy = new ListNode(0);
-        ListNode temp = dummy;
-        ListNode l1 = list1;
-        ListNode l2 = list2;
+        ListNode dm = dummy;
 
-        while(l1!=null && l2!=null){
-            if(l1.val<l2.val){
-                temp.next = l1;
-                l1 = l1.next;
-            }else if(l2.val<l1.val){
-                temp.next = l2;
-                l2 = l2.next;
+        ListNode t1 = l1;
+        ListNode t2 = l2;
+
+        while(t1!=null && t2!=null){
+            if(t1.val<=t2.val){
+                dm.next = t1;
+                t1 = t1.next;
             }else{
-                temp.next = l1;
-                l1 = l1.next;
+                dm.next = t2;
+                t2=t2.next;
             }
-            temp = temp.next;
+            dm = dm.next;
         }
-
-        while(l1!=null){
-            temp.next = l1;
-            l1 = l1.next;
-            temp = temp.next;
-        }
-        while(l2!=null){
-            temp.next = l2;
-            l2 = l2.next;
-            temp = temp.next;
-        }
+        if(t1!=null) dm.next = t1;
+        if(t2!=null) dm.next = t2;
         return dummy.next;
-    }
-    public ListNode partisanAndMerge(int s,int e,ListNode[] list){
-        if(s>e) return null;
-        if(s==e){
-            return list[s];
-        }
-        int mid = s+(e-s)/2;
-        ListNode l1 = partisanAndMerge(s,mid,list);
-        ListNode l2 = partisanAndMerge(mid+1,e,list);
-        return merge(l1,l2);
     }
     public ListNode mergeKLists(ListNode[] lists) {
         int n = lists.length;
         if(n==0) return null;
-        return partisanAndMerge(0,n-1,lists);
+        ListNode h1 = lists[0];
+        for(int i=1;i<n;i++){
+            ListNode h2 = lists[i];
+            h1 = merge(h1,h2);
+        }
+        return h1;
+        
     }
 }
