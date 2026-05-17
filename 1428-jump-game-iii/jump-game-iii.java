3a5e922c0c0af76758bcae27f44d7413bcf28f32
Anuj Yadav <anujyadav992241@gmail.com>
2026-05-17 13:18:53 +0530

    Time: 0 ms (100.00%) | Memory: 58.5 MB (16.90%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.java
+++ 1428-jump-game-iii/jump-game-iii.java
@@ -1,17 +1,19 @@
 class Solution {
-    public boolean solve(int[] arr,int idx){
+    public boolean solve(int[] arr,int idx,Boolean[] dp){
         int n = arr.length;
         if(idx<0 || idx>n-1||arr[idx]<0) return false;
+        if(dp[idx]!=null) return dp[idx];
         if(arr[idx] == 0) return true;
         arr[idx] = -arr[idx];
-        boolean l = solve(arr,idx-arr[idx]);
-        boolean r = solve(arr,idx+arr[idx]);
-        return l||r;
+        boolean l = solve(arr,idx-arr[idx],dp);
+        boolean r = solve(arr,idx+arr[idx],dp);
+        return dp[idx] = l||r;
     }
     public boolean canReach(int[] arr, int start) {
         int n = arr.length;
         if(arr[n-1]==0) return true;
         if(arr[start]==0) return true;
-        return solve(arr,start);
+        Boolean dp[] = new Boolean[n+1];
+        return solve(arr,start,dp);
     }
 }
