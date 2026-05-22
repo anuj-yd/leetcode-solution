2a00f1deb8013cabf673641dc9ee8ea80f477e05
Anuj Yadav <anujyadav992241@gmail.com>
2026-05-23 01:26:01 +0530

    Time: 31 ms (6.89%) | Memory: 98.2 MB (13.67%) - LeetSync

---
+++ 77-combinations/combinations.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void solve(int n,int idx,int k,List<List<Integer>> ans,List<Integer> comb){
+        
+        if(idx>n){
+            if(k==0){
+                ans.add(new ArrayList<>(comb));
+            }
+            return;
+        }
+
+        comb.add(idx);
+        solve(n,idx+1,k-1,ans,comb);
+        comb.remove(comb.size()-1);
+        solve(n,idx+1,k,ans,comb);
+
+    }
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> comb = new ArrayList<>();
+        solve(n,1,k,ans,comb);
+        return ans;
+    }
+}
