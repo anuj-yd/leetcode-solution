e42427af676b9714a72dfa294fcacb6b577f7264
Anuj Yadav <anujyadav992241@gmail.com>
2026-05-25 00:44:13 +0530

    Time: 11 ms (50.29%) | Memory: 46.3 MB (58.86%) - LeetSync

--- 1466-jump-game-v/jump-game-v.java
+++ 1466-jump-game-v/jump-game-v.java
@@ -1,8 +1,8 @@
 class Solution {
-    public int solve(int[] arr,int idx,int d,int[][] dp){
+    public int solve(int[] arr,int idx,int d,int[] dp){
         int n = arr.length;
         if(idx>=n) return 0;
-        if(dp[idx][d]!=-1) return dp[idx][d];
+        if(dp[idx]!=-1) return dp[idx];
         int res = 1;
         for(int j=1;j<=d;j++){
             if(idx+j>=n || arr[idx+j]>=arr[idx]) break;
@@ -15,15 +15,17 @@ class Solution {
             res = (int)Math.max(res,ljump);
         }
        
-        return dp[idx][d]=res;
+        return dp[idx]=res;
     }
     public int maxJumps(int[] arr, int d) {
         int n = arr.length;
         int ans = Integer.MIN_VALUE;
-        int[][] dp = new int[n+1][d+1];
-        for(int[] sd : dp){
-            Arrays.fill(sd,-1);
-        }
+        // int[][] dp = new int[n+1][d+1];
+        // for(int[] sd : dp){
+        //     Arrays.fill(sd,-1);
+        // }
+        int dp[] = new int[n+1];
+        Arrays.fill(dp,-1);
         for(int i=0;i<n;i++){
             ans = (int)Math.max(ans,solve(arr,i,d,dp));
         }
