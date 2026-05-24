06daeb3f649322a6d2285af5fa81c1de4d580ae5
Anuj Yadav <anujyadav992241@gmail.com>
2026-05-25 00:42:31 +0530

    Time: 27 ms (11.43%) | Memory: 58.3 MB (5.43%) - LeetSync

---
+++ 1466-jump-game-v/jump-game-v.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int solve(int[] arr,int idx,int d,int[][] dp){
+        int n = arr.length;
+        if(idx>=n) return 0;
+        if(dp[idx][d]!=-1) return dp[idx][d];
+        int res = 1;
+        for(int j=1;j<=d;j++){
+            if(idx+j>=n || arr[idx+j]>=arr[idx]) break;
+            int rjump = 1+solve(arr,idx+j,d,dp);
+            res = (int)Math.max(res,rjump);
+        }
+        for(int j=1;j<=d;j++){
+            if(idx-j<0 || arr[idx-j]>=arr[idx]) break;
+            int ljump = 1+solve(arr,idx-j,d,dp);
+            res = (int)Math.max(res,ljump);
+        }
+       
+        return dp[idx][d]=res;
+    }
+    public int maxJumps(int[] arr, int d) {
+        int n = arr.length;
+        int ans = Integer.MIN_VALUE;
+        int[][] dp = new int[n+1][d+1];
+        for(int[] sd : dp){
+            Arrays.fill(sd,-1);
+        }
+        for(int i=0;i<n;i++){
+            ans = (int)Math.max(ans,solve(arr,i,d,dp));
+        }
+        return ans==Integer.MIN_VALUE ? 0 : ans;
+        
+    }
+}
