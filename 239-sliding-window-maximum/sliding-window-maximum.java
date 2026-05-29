3788717e79448b32d5c5af0a4ad676ad880cbee3
Anuj Yadav <anujyadav992241@gmail.com>
2026-05-29 10:32:40 +0530

    Time: 76 ms (12.80%) | Memory: 158.5 MB (6.33%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,26 +1,35 @@
 class Solution {
     public int[] maxSlidingWindow(int[] arr, int k) {
-        int i = 0;
-        int j=0;
+
         int n = arr.length;
-        Deque<Integer> q = new ArrayDeque<>();
-        int res[] = new int[n-k+1];
-        while(j<arr.length){
-           while (!q.isEmpty() && arr[j]>q.getLast()){
-                q.pollLast(); 
-            }
-            q.addLast(arr[j]);
-            if(j-i+1<k){
+        int[] res = new int[n - k + 1];
+
+        PriorityQueue<int[]> pq =
+            new PriorityQueue<>((a, b) -> b[0] - a[0]);
+
+        int i = 0, j = 0, idx = 0;
+
+        while (j < n) {
+
+            pq.offer(new int[]{arr[j], j});
+
+            if (j - i + 1 < k) {
                 j++;
-            }else if(j-i+1 == k){
-                res[i] = q.peek();
-                if (arr[i] == q.peek()) {
-                    q.poll();
+            }
+            else {
+
+                // stale elements remove
+                while (!pq.isEmpty() && pq.peek()[1] < i) {
+                    pq.poll();
                 }
+
+                res[idx++] = pq.peek()[0];
+
                 i++;
                 j++;
             }
         }
+
         return res;
     }
 }
