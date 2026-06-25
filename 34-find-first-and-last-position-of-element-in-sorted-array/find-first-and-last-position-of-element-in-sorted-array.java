fe321394d507052e792aed094a795f686624436e
Anuj Yadav <anujyadav992241@gmail.com>
2026-06-25 15:57:11 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (69.97%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,43 +1,44 @@
 class Solution {
-    private int first(int[] nums, int target){
-        int low  = 0;
-        int high = nums.length-1;
-        int ans = -1;
+    public int solve1(int[] arr,int target){
+        int low = 0;
+        int high = arr.length-1;
+        int idx = -1;
         while(low<=high){
             int mid = low+(high-low)/2;
-            if(nums[mid]==target){
-                ans = mid;
+            if(arr[mid]==target){
+                idx = mid;
                 high = mid-1;
-            }else if(nums[mid]>target){
+            }else if(arr[mid]>target){
                 high = mid-1;
             }else{
                 low = mid+1;
-            }
+            }  
         }
-        return ans;
+        return idx;
     }
-    private int last(int[] nums, int target){
-        int low  = 0;
-        int high = nums.length-1;
-        int ans = -1;
+    public int solve2(int[] arr,int target){
+        int low = 0;
+        int high = arr.length-1;
+        int idx = -1;
         while(low<=high){
             int mid = low+(high-low)/2;
-            if(nums[mid]==target){
-                ans = mid;
+            if(arr[mid]==target){
+                idx = mid;
+                
                 low = mid+1;
-            }else if(nums[mid]>target){
+            }else if(arr[mid]>target){
                 high = mid-1;
             }else{
                 low = mid+1;
-            }
+            }  
         }
-        return ans;
+        return idx;
     }
+    
     public int[] searchRange(int[] nums, int target) {
-        int ans[] = new int[2];
-        ans[0] = first(nums,target);
-        ans[1] = last(nums,target);
-        return ans;
+        int first = solve1(nums,target);
+        int last = solve2(nums,target);
+        return new int[]{first,last};
         
     }
 }
