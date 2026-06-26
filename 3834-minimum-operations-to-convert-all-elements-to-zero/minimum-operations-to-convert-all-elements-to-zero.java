919608205101e2db2435db7ba39fd1a567afba41
Anuj Yadav <anujyadav992241@gmail.com>
2026-06-26 15:43:46 +0530

    Time: 37 ms (70.62%) | Memory: 123 MB (93.79%) - LeetSync

--- 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.java
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.java
@@ -1,42 +1,22 @@
-// import java.util.Stack;
-
-// class Solution {
-//     public int minOperations(int[] nums) {
-//         Stack<Integer> st = new Stack<>();
-//         int ops = 0;
-//         for (int num : nums) {
-//             if (num == 0){
-//                 st.clear();
-//                 continue;
-//             }
-//             // remove any values greater than current num
-//             while (!st.isEmpty() && st.peek() > num) {
-//                 st.pop();
-//             }
-//             if (st.isEmpty() || st.peek() < num) {
-//                 st.push(num);
-//                 ops++; // each push corresponds to one operation
-//             }    
-//         }
-//         return ops;
-//     }
-// }
-
-
 class Solution {
     public int minOperations(int[] nums) {
-        var stack = new int[nums.length + 1];
-        var top = 0;
-        var ans = 0;
-        for (var i = 0; i < nums.length; i++) {
-            while (stack[top] > nums[i]) {
-                top--;
+        int ans = 0;
+        Deque<Integer> st = new ArrayDeque<>();
+
+        st.push(0);
+
+        for (int num : nums) {
+
+            while (!st.isEmpty() && st.peek() > num) {
+                st.pop();
+            }
+
+            if (st.isEmpty() || st.peek() < num) {
+                st.push(num);
                 ans++;
             }
-            if (stack[top] != nums[i])
-                stack[++top] = nums[i];
         }
-        return ans + top;
+
+        return ans;
     }
 }
-
