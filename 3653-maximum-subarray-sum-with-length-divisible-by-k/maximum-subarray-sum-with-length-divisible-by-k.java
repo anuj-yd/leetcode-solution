c9c24408c147051616e4c781edef9a8dab58bee3
Anuj Yadav <anujyadav992241@gmail.com>
2026-06-29 14:46:20 +0530

    Time: 5 ms (56.81%) | Memory: 153.7 MB (42.25%) - LeetSync

--- 3653-maximum-subarray-sum-with-length-divisible-by-k/maximum-subarray-sum-with-length-divisible-by-k.java
+++ 3653-maximum-subarray-sum-with-length-divisible-by-k/maximum-subarray-sum-with-length-divisible-by-k.java
@@ -1,25 +1,35 @@
 class Solution {
     public long maxSubarraySum(int[] nums, int k) {
-        long[] minPrefix = new long[k];
-        final long INF = Long.MAX_VALUE;
+        int n = nums.length;
 
-        for (int i = 0; i < k; i++) minPrefix[i] = INF;
-        minPrefix[0] = 0;
+        long[] prefSum = new long[n];
+        prefSum[0] = nums[0];
 
-        long prefix = 0;
-        long answer = Long.MIN_VALUE;
-
-        for (int i = 0; i < nums.length; i++) {
-            prefix += nums[i];
-            int mod = (i + 1) % k;
-
-            if (minPrefix[mod] != INF) {
-                answer = Math.max(answer, prefix - minPrefix[mod]);
-            }
-
-            minPrefix[mod] = Math.min(minPrefix[mod], prefix);
+        for (int i = 1; i < n; i++) {
+            prefSum[i] = prefSum[i - 1] + nums[i];
         }
 
-        return answer;
+        long result = Long.MIN_VALUE;
+
+        for (int start = 0; start < k; start++) {
+
+            long currSum = 0;
+            int i = start;
+
+            while (i + k - 1 < n) {
+
+                int j = i + k - 1;
+
+                long subSum = prefSum[j] - (i > 0 ? prefSum[i - 1] : 0);
+
+                currSum = Math.max(subSum, currSum + subSum);
+
+                result = Math.max(result, currSum);
+
+                i += k;
+            }
+        }
+
+        return result;
     }
 }
