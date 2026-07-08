8133bbe824abb80b2cff6fa9d83020166e78bc4b
Anuj Yadav <anujyadav992241@gmail.com>
2026-07-08 09:42:15 +0530

    Time: 1 ms (97.35%) | Memory: 43.5 MB (86.04%) - LeetSync

--- 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.java
+++ 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.java
@@ -1,41 +1,55 @@
 class Solution {
-    int prnt[] = new int[26];
-    public int find(int x){
-        if(prnt[x]!=x){
-            prnt[x] = find(prnt[x]);
+    class DSU{
+        int prnt[];
+        int rnk[];
+
+        DSU(int v){
+            prnt = new int[v];
+            rnk = new int[v];
+            for(int i=0;i<v;i++){
+                prnt[i]=i;
+            }
         }
-        return prnt[x];
-    }
 
-    public void union(int x,int y){
-        int px = find(x);
-        int py = find(y);
+        public int find(int x){
+            if(prnt[x] == x) return x;
+            return prnt[x] = find(prnt[x]);
+        }
 
-        if(px!=py){
-            prnt[px] = py;
+        public void union(int x,int y){
+            int px = find(x);
+            int py = find(y);
+
+            if(px==py) return;
+            if(rnk[px]>py){
+                prnt[py] =  px;
+            }else if(rnk[py]>px){
+                prnt[px] = py;
+            }else{
+                prnt[px] = py;
+                rnk[py]++;
+            }
         }
     }
     public boolean equationsPossible(String[] equations) {
-        for(int i=0;i<26;i++){
-            prnt[i] = i;
-        }
-        for(String s : equations){
-            if(s.charAt(1)=='='){
-                int a = s.charAt(0)-'a';
-                int b = s.charAt(3)-'a';
-                union(a,b);
+        DSU d = new DSU(26);
+
+        for(String eq : equations){
+            if(eq.charAt(1)=='='){
+                int a = eq.charAt(0)-'a';
+                int b = eq.charAt(3)-'a';
+
+                d.union(a,b);
             }
         }
-        for(String s : equations){
-            if(s.charAt(1)=='!'){
-                int a = s.charAt(0)-'a';
-                int b = s.charAt(3)-'a';
-                if(find(a)==find(b)) return false;
+        for(String eq : equations){
+            if(eq.charAt(1)=='!'){
+                int a = eq.charAt(0)-'a';
+                int b = eq.charAt(3)-'a';
+                if(d.find(a)==d.find(b)) return false;
             }
         }
         return true;
-
-
         
     }
 }
