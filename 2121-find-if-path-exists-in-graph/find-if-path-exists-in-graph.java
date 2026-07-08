9d545a59bad0fac14d20734d16008a74e57ec2a0
Anuj Yadav <anujyadav992241@gmail.com>
2026-07-08 12:28:42 +0530

    Time: 18 ms (92.53%) | Memory: 240.5 MB (95.58%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,29 +1,53 @@
-import java.util.*;
-
 class Solution {
-    public boolean validPath(int n, int[][] edges, int source, int destination) {
-        List<List<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
-        for (int[] edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            adj.get(u).add(v);
-            adj.get(v).add(u); 
-        }
-        boolean[] visited = new boolean[n];
-        return dfs(source, destination, visited, adj);
-    }
+    class DSU{
+        int prnt[];
+        int rnk[];
 
-    private boolean dfs(int curr, int dest, boolean[] visited, List<List<Integer>> adj) {
-        if (curr == dest) return true;
-
-        visited[curr] = true;
-
-        for (int nbr : adj.get(curr)) {
-            if (!visited[nbr]) {
-                if (dfs(nbr, dest, visited, adj)) return true;
+        DSU(int n){
+            prnt= new int[n];
+            rnk = new int[n];
+            for(int i=0;i<n;i++){
+                prnt[i] = i;
             }
         }
+
+        public int find(int x){
+            if(x == prnt[x]) return x;
+            return prnt[x] = find(prnt[x]);
+        }
+        public void union(int x,int y){
+
+            int px = find(x);
+            int py = find(y);
+
+            if(px == py) return;
+
+            if(rnk[px]>rnk[py]){
+                prnt[py] = px;
+
+            }else if(rnk[py]>rnk[px]){
+                prnt[px] = py;
+            }else{
+                prnt[px] = py;
+                rnk[py]++;
+            }
+
+
+        }
+    }
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        DSU d = new DSU(n);
+
+        for(int ed[] : edges){
+            int u = ed[0];
+            int v = ed[1];
+
+            if(d.find(u)!=d.find(v)){
+                d.union(u,v);
+            }
+        }
+        if(d.find(source)==d.find(destination)) return true;
+
         return false;
     }
 }
