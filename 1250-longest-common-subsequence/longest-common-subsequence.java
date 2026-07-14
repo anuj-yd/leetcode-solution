a1b7c5acbfd502efe5cd816a44f72c0f8a4507ea
Anuj Yadav <anujyadav992241@gmail.com>
2026-07-14 12:58:33 +0530

    Time: 31 ms (11.28%) | Memory: 58.5 MB (5.06%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,25 +1,21 @@
 class Solution {
-    int count = 0;
-    private int helper(int i,int j,String t1,String t2,Integer[][] dp){
-        
-        if(i<0 || j<0) return 0;
-        int count = 0;
-        if(dp[i][j] != null) return dp[i][j]; 
-        if(t1.charAt(i)==t2.charAt(j)){
-            count = 1+ helper(i-1,j-1,t1,t2,dp);
-        }else if(t1.charAt(i)!=t2.charAt(j)){
-            int la = helper(i-1,j,t1,t2,dp);
-            int ra = helper(i,j-1,t1,t2,dp);
-            int max = Math.max(la,ra);
-            if(max>count) count = max;
+
+    public int solve(int i,int j,int m,int n,String text1, String text2,Integer[][] dp){
+        if( i>= m || j >= n) return 0;
+        if(dp[i][j]!=null) return dp[i][j];
+
+        if(text1.charAt(i)==text2.charAt(j)){
+            return 1+solve(i+1,j+1,m,n,text1,text2,dp);
         }
-        return dp[i][j]=count;
+
+        int picki = solve(i+1,j,m,n,text1,text2,dp);
+        int pickj = solve(i,j+1,m,n,text1,text2,dp);
+
+        return dp[i][j]=Math.max(picki,pickj);
     }
     public int longestCommonSubsequence(String text1, String text2) {
-        int i = text1.length()-1;
-        int j = text2.length()-1;
-        Integer[][] dp = new Integer[i+1][j+1];
-        return helper(i,j,text1,text2,dp);
-        
+        Integer[][] dp = new Integer[text1.length()][text2.length()];
+
+        return solve(0,0,text1.length(),text2.length(),text1,text2,dp);
     }
 }
