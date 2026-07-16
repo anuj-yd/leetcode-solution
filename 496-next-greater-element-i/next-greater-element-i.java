c6ff53de6e7d81347c98a7cbf275346d2af9ebcd
Anuj Yadav <anujyadav992241@gmail.com>
2026-07-16 12:16:56 +0530

    Time: 4 ms (74.70%) | Memory: 45.4 MB (36.61%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,24 +1,27 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-
         Stack<Integer> st = new Stack<>();
         Map<Integer,Integer> map = new HashMap<>();
-
         for(int num : nums2){
-            while(!st.isEmpty() && num>= st.peek()){
+            while(!st.isEmpty() && num>=st.peek()){
                 map.put(st.pop(),num);
             }
-            st.add(num);
+            st.push(num);
         }
         while(!st.isEmpty()){
             map.put(st.pop(),-1);
         }
-
-        int[] ans = new int[nums1.length];
-        for(int i =0;i<ans.length;i++){
+        int ans[] = new int[nums1.length];
+        for(int i=0;i<ans.length;i++){
             ans[i] = map.get(nums1[i]);
         }
+
         return ans;
+
+         
+
+
+
         
     }
 }
