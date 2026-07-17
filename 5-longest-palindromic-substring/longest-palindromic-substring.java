62acae6423715905dd5bbb893f8051a45debd7f9
Anuj Yadav <anujyadav992241@gmail.com>
2026-07-17 12:36:56 +0530

    Time: 174 ms (21.76%) | Memory: 50.7 MB (5.43%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,25 +1,33 @@
 class Solution {
-    public boolean isPalin(int i,int j,String s,Boolean[][] dp){
-        if(i>=j) return true;
 
-        if(dp[i][j]!=null) return dp[i][j];
-
-        if(s.charAt(i)==s.charAt(j)){
-            return dp[i][j] = isPalin(i+1,j-1,s,dp);
-        }
-
-        return dp[i][j] = false;
-    }
     public String longestPalindrome(String s) {
         int n = s.length();
-        Boolean[][] dp = new Boolean[1001][1001];
+        boolean[][] dp = new boolean[1001][1001];
 
         int sp = -1;
         int len = Integer.MIN_VALUE;
 
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(isPalin(i,j,s,dp) && j-i+1 > len){
+        for(int l=1;l<=n;l++){
+            for(int i=0;i+l-1<n;i++){
+
+                int j = i+l-1;
+
+                //l1
+                if(i==j){
+                    dp[i][j]=true;
+                }
+                //l2
+                else if(i+1==j){
+                    dp[i][j] = s.charAt(i)==s.charAt(j);
+                }
+                //l>=3
+                else{
+
+                    dp[i][j] = (s.charAt(i)==s.charAt(j)) && dp[i+1][j-1];
+
+                }
+
+                if(dp[i][j]==true){
                     sp = i;
                     len = j-i+1;
                 }
