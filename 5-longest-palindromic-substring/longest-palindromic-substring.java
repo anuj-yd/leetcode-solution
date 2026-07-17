5f241ca504408cf7f89408a03ae4a861c89c7b49
Anuj Yadav <anujyadav992241@gmail.com>
2026-07-17 12:29:21 +0530

    Time: 281 ms (18.75%) | Memory: 277.9 MB (5.43%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,31 +1,32 @@
 class Solution {
-    public boolean solve(int i,int j,String s){
-        while(i<=j){
-            if(s.charAt(i)==s.charAt(j)){
-                i++;
-                j--;
-            }else{
-                return false;
-            }
-        }
-        return true;
+    public boolean isPalin(int i,int j,String s,Boolean[][] dp){
+        if(i>=j) return true;
 
+        if(dp[i][j]!=null) return dp[i][j];
+
+        if(s.charAt(i)==s.charAt(j)){
+            return dp[i][j] = isPalin(i+1,j-1,s,dp);
+        }
+
+        return dp[i][j] = false;
     }
     public String longestPalindrome(String s) {
-        int maxLen = Integer.MIN_VALUE;
-        int sp = -1;
+        int n = s.length();
+        Boolean[][] dp = new Boolean[1001][1001];
 
-        for(int i=0;i<s.length();i++){
-            for(int j=i;j<s.length();j++){
-                if(solve(i,j,s)){
-                    if(j-i+1>maxLen){
-                        maxLen = j-i+1;
-                        sp = i;
-                    }
+        int sp = -1;
+        int len = Integer.MIN_VALUE;
+
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(isPalin(i,j,s,dp) && j-i+1 > len){
+                    sp = i;
+                    len = j-i+1;
                 }
             }
         }
-        return (sp!=-1 && maxLen!=Integer.MIN_VALUE) ? s.substring(sp,sp+maxLen) : "";
+
+        return (sp!=-1 && len!=Integer.MIN_VALUE) ? s.substring(sp,sp+len) : "";
         
     }
 }
