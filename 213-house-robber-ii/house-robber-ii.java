f13a9a2e3f84bef351d4dbf96d98ef8a70bceea6
Anuj Yadav <anujyadav992241@gmail.com>
2026-07-20 11:59:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (61.14%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,25 +1,25 @@
 class Solution {
-    public int solve(int[] arr,int idx,int n,int[] dp){
+    public int solve(int i,int nums[],int n,int dp[]){
+        if(i>n) return 0;
 
-        if(idx>n) return 0;
-        if(dp[idx]!=-1) return dp[idx];
+        if(dp[i]!=-1) return dp[i];
 
-        int pick = arr[idx]+solve(arr,idx+2,n,dp);
-        int skip = solve(arr,idx+1,n,dp);
+        int pick = nums[i] + solve(i+2,nums,n,dp);
+        int skip = solve(i+1,nums,n,dp);
 
-        return dp[idx] = Math.max(pick,skip); 
-        
-
+        return dp[i] = Math.max(pick,skip);
     }
     public int rob(int[] nums) {
         int n = nums.length;
         if (n == 1) return nums[0];
-        int[] dp1 = new int[n];
+        int[] dp1 = new int[101];
         Arrays.fill(dp1,-1);
-        int sum1 = solve(nums,0,n-2,dp1);
-        int[] dp2 = new int[n];
+        int[] dp2 = new int[101];
         Arrays.fill(dp2,-1);
-        int sum2 = solve(nums,1,n-1,dp2);
-        return Math.max(sum1,sum2);
+        int s1 = solve(0,nums,n-2,dp1);
+        int s2 = solve(1,nums,n-1,dp2);
+
+        return Math.max(s1,s2);
+        
     }
 }
