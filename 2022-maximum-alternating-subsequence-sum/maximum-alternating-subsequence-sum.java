9db89c7d44f2848417e38384e688dbd015b18d6c
Anuj Yadav <anujyadav992241@gmail.com>
2026-07-20 14:35:45 +0530

    Time: 28 ms (74.08%) | Memory: 104.4 MB (62.96%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.java
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.java
@@ -1,27 +1,17 @@
 class Solution {
-    public long solve(int i,int[] nums,long[][] dp,int isEv){
-        if(i==nums.length) return 0;
-
-        if(dp[i][isEv]!=-1) return dp[i][isEv];
-
-        long pick = 0;
-
-        if(isEv==0){
-            pick = nums[i]+solve(i+1,nums,dp,1);
-        }else{
-            pick = -nums[i]+solve(i+1,nums,dp,0);
-        }
-
-        long skip = solve(i+1,nums,dp,isEv);
-
-        return dp[i][isEv] = Math.max(pick,skip);
-    }
     public long maxAlternatingSum(int[] nums) {
-
         int n = nums.length;
         long[][] dp = new long[n+1][2];
-        for(long[] d : dp) Arrays.fill(d,-1);
-        return solve(0,nums,dp,0);
-        
+
+        for(int i=1;i<=n;i++){
+            for(int isEv = 0;isEv<2;isEv++){
+                if(isEv==0){
+                    dp[i][isEv] = Math.max(-nums[i-1]+dp[i-1][1],dp[i-1][isEv]);
+                }else{
+                    dp[i][isEv] = Math.max(nums[i-1]+dp[i-1][0],dp[i-1][isEv]);
+                }
+            }
+        }
+        return Math.max(dp[n][0],dp[n][1]);
     }
 }
