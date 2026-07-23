4c6beeeae779f3249348f9b7755fed3f6d356623
Anuj Yadav <anujyadav992241@gmail.com>
2026-07-23 15:23:27 +0530

    Time: 48 ms (34.76%) | Memory: 45.9 MB (83.34%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,27 +1,38 @@
 class Solution {
 
-    public int solve(int pi,int ci,int[] nums,int[][] dp){
-        if(ci==nums.length) return 0;
+    // public int solve(int pi,int ci,int[] nums,int[][] dp){
+    //     if(ci==nums.length) return 0;
 
-        if(dp[pi+1][ci]!=-1) return dp[pi+1][ci];
+    //     if(dp[pi+1][ci]!=-1) return dp[pi+1][ci];
 
-        int pick = 0;
+    //     int pick = 0;
 
-        if(pi==-1 || nums[pi]<nums[ci]){
-            pick = 1+solve(ci,ci+1,nums,dp);
-        }
-        int skip = solve(pi,ci+1,nums,dp);
+    //     if(pi==-1 || nums[pi]<nums[ci]){
+    //         pick = 1+solve(ci,ci+1,nums,dp);
+    //     }
+    //     int skip = solve(pi,ci+1,nums,dp);
 
-        return dp[pi+1][ci] = Math.max(pick,skip);
-    }
+    //     return dp[pi+1][ci] = Math.max(pick,skip);
+    // }
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int dp[][] = new int[n][n];
+        // int dp[][] = new int[n][n];
+        int t[] = new int[n];
+        Arrays.fill(t,1);
 
+        // for(int[] d : dp) Arrays.fill(d,-1);
+        int maxLen = 1;
+        for(int i=0;i<n;i++){
 
-        for(int[] d : dp) Arrays.fill(d,-1);
-
-        return solve(-1,0,nums,dp);
+           for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    t[i] = Math.max(t[i],t[j]+1);
+                    maxLen = Math.max(maxLen,t[i]);
+                }
+           }
+            
+        }
+        return maxLen;
         
     }
 }
