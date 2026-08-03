1e3f5e7bb4c264b927b3f2238700e68ed633f669
Anuj Yadav <anujyadav992241@gmail.com>
2026-08-04 01:21:04 +0530

    Time: 34 ms (47.24%) | Memory: 92 MB (45.98%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.java
+++ 1522-stone-game-iii/stone-game-iii.java
@@ -2,17 +2,19 @@ class Solution {
     public int solve(int idx,int n,int nums[],int[] dp){
         if(idx==n) return 0;
         if(dp[idx]!=Integer.MIN_VALUE) return dp[idx];
+
         int pick = 0;
+
         for(int i=idx;i<Math.min(idx+3,n);i++){
             pick += nums[i];
             dp[idx] = Math.max(dp[idx], pick - solve(i + 1, n, nums,dp));
         }
+
         return dp[idx];
     }
     public String stoneGameIII(int[] stoneValue) {
-
         int n = stoneValue.length;
-        int[] dp = new int[n];
+        int[] dp = new int[50000];
         Arrays.fill(dp,Integer.MIN_VALUE);
 
         int ans = solve(0,n,stoneValue,dp);
@@ -20,11 +22,9 @@ class Solution {
         if(ans > 0){
             return "Alice";
         }else if(ans < 0){
-
             return "Bob";
         }else{
             return "Tie";
         }
-        
     }
 }
