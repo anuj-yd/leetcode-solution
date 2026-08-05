5fddfe6a0a3d207008c70f979971018b7b8db270
Anuj Yadav <anujyadav992241@gmail.com>
2026-08-06 00:24:30 +0530

    Time: 77 ms (53.85%) | Memory: 289.9 MB (45.19%) - LeetSync

---
+++ 3561-remove-methods-from-project/remove-methods-from-project.java
@@ -0,0 +1,45 @@
+class Solution {
+    public void dfs(boolean[] vis,List<List<Integer>> adj,int node){
+        vis[node] = true;
+
+        for(int nbr : adj.get(node)){
+            if(!vis[nbr]){
+                dfs(vis,adj,nbr);
+            }
+        }
+    }
+
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
+        for(int ed[] : invocations){
+            adj.get(ed[0]).add(ed[1]);
+        }
+
+        boolean vis[] = new boolean[n];
+        dfs(vis,adj,k);
+
+        for(int[] ed : invocations){
+            int u = ed[0];
+            int v = ed[1];
+            if (!vis[u] && vis[v]){
+                List<Integer> ans = new ArrayList<>();
+                for(int i = 0; i < n; i++) {
+                    ans.add(i);
+                }
+                return ans;
+            }
+        }
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                ans.add(i);
+            }
+        }
+
+        return ans;
+    }
+
+
+}
