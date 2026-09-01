b69c1bec8f46900c580ce8260d764cd99420b4e5
Anuj Yadav <anujyadav992241@gmail.com>
2026-09-02 01:29:42 +0530

    Time: 585 ms (29.41%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,4 +1,7 @@
-SELECT name
-FROM Customer
-WHERE referee_id != 2
-   OR referee_id IS NULL;
+# Write your MySQL query statement below
+SELECT c.name
+FROM Customer c
+LEFT JOIN Customer rf
+    ON c.referee_id = rf.id
+WHERE rf.id != 2
+   OR rf.id IS NULL;
