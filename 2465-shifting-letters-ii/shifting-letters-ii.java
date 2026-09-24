e50bcd8d807bd317cf7b292671493a1db57df201
Anuj Yadav <anujyadav992241@gmail.com>
2026-09-25 00:19:31 +0530

    Time: 9 ms (16.69%) | Memory: 113.1 MB (76.44%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.java
+++ 2465-shifting-letters-ii/shifting-letters-ii.java
@@ -1,34 +1,54 @@
 class Solution {
     public String shiftingLetters(String s, int[][] shifts) {
-        int dat[] = new int[s.length()];
 
-        for(int sh[] : shifts){
-            int l = sh[0];
-            int r = sh[1];
+        int[] dat = new int[s.length()];
 
-            int x = (sh[2] == 1) ? 1 : -1;
+        for(int i=0;i<shifts.length;i++){
+            
+            int l = shifts[i][0];
+            int r = shifts[i][1];
+            int d = shifts[i][2];
+
+            int x = 0;
+            x = (d == 0) ? -1 : 1;
 
             dat[l] += x;
-            if(r+1<s.length()){
-                dat[r+1]-=x;
-            }
+            if(r+1<s.length()) dat[r+1] -= x;
+            
         }
 
-        for(int i=1;i<dat.length;i++){
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=1;i<s.length();i++){
             dat[i] = dat[i-1]+dat[i];
         }
-        char[] arr = s.toCharArray();
+
         for(int i=0;i<s.length();i++){
-            int shift = dat[i]%26;
-            if(shift<0){
-                shift+=26;
-            }
-            arr[i] = (char) (((arr[i] - 'a' + shift) % 26) + 'a');
+            int ascii = (int) s.charAt(i);
+            int newascii = ascii + (dat[i]%26);
+
+           if(newascii < 97){
+            int dif = 97 - newascii;
+            newascii = 122 - (dif-1);
+           }else if(newascii > 122){
+
+            int dif = newascii - 122;
+            newascii = 97 + (dif-1);
+
+           }
+
+           sb.append((char)newascii);
+
         }
-        return new String(arr);
+        return sb.toString();
+
 
         
+
+
+       
         
+    
         
     }
 }
