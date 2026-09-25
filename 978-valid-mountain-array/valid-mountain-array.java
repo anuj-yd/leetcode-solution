8aa601df21cc1114fe127d497f930f7f44f95f4b
Anuj Yadav <anujyadav992241@gmail.com>
2026-09-25 17:36:44 +0530

    Time: 1 ms (99.89%) | Memory: 47.7 MB (17.01%) - LeetSync

--- 978-valid-mountain-array/valid-mountain-array.java
+++ 978-valid-mountain-array/valid-mountain-array.java
@@ -1,45 +1,18 @@
 class Solution {
     public boolean validMountainArray(int[] arr) {
+        
+        int i = 0;
         int n = arr.length;
-        if(n<3) return false;
-        boolean peak = false;
-
-        for(int i=1;i<n;i++){
-            // boolean l=false;
-            // boolean r=false;
-            // if(arr[i-1]<arr[i]) l=true;
-            // if(i+1==n || arr[i]>arr[i+1]) r=true;
-
-            // // if(i+1<n && arr[i-1]<arr[i] && arr[i]>arr[i+1]){
-            // //     if(peak) return false;
-            // //     peak = true;
-            // // }
-            // if()
-            // if(l && r){
-            //     if(peak) return false;
-            //     peak=true;
-            // }
-
-            if(!peak){
-                if(arr[i]<=arr[i-1]) return false;
-            }else{
-                if(i+1<n && arr[i]<=arr[i+1]) return false;
-            }
-            
-
-            boolean l=false;
-            boolean r=false;
-            if(arr[i-1]<arr[i]) l=true;
-            if(i+1<n && arr[i]>arr[i+1]) r=true;
-
-            if(l && r){
-                // if(peak) return false;
-                peak=true;
-            }
-            
-            
-        }
-        return peak;
-
+        
+        while (i + 1 < n && arr[i] < arr[i+1])
+            i++;
+        
+        if (i == 0 || i == n - 1) 
+            return false;
+        
+        while (i + 1 < n && arr[i] > arr[i + 1])
+            i++;
+        
+        return i == n-1;
     }
 }
