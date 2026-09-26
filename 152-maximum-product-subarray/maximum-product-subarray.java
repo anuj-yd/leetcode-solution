5d6690360bc030dd8009d83f3a9f84a1d307ede2
Anuj Yadav <anujyadav992241@gmail.com>
2026-09-26 19:29:02 +0530

    Time: 1 ms (93.29%) | Memory: 47.2 MB (52.94%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -4,25 +4,28 @@ class Solution {
         int ppro = 1;
         int spro = 1;
 
-        for(int val : nums){
-            if(ppro == 0){
-                ppro = 1;
-            }
-            ppro *= val; 
-            if(ppro>maxPro) maxPro = ppro;
+        // for(int val : nums){
+        //     if(ppro == 0) ppro = 1;
+        //     ppro *= val; 
+        //     if(ppro>maxPro) maxPro = ppro;
+        // }
+
+        // for(int i=nums.length-1;i>=0;i--){
+        //     if(spro == 0) spro = 1;
+        //     spro *= nums[i];
+        //     if(spro>maxPro) maxPro = spro;
+        // }
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            ppro *= nums[i];
+            spro *= nums[n - 1 - i];
+
+            maxPro = Math.max(maxPro, Math.max(ppro, spro));
+
+            if (ppro == 0) ppro = 1;
+            if (spro == 0) spro = 1;
         }
-
-        for(int i=nums.length-1;i>=0;i--){
-            if(spro == 0){
-                spro = 1;
-            }
-
-            spro *= nums[i];
-
-            if(spro>maxPro) maxPro = spro;
-
-        }
-
         return maxPro;
     }
 }
